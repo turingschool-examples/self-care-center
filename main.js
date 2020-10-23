1382250a7a2880a113c95830f4b04f1275668a46
Elsa Fluss <elsafluss@gmail.com>
2020-10-22 18:32:11 -0600

    Made text responsive to screen size.

--- main.js
+++ main.js
@@ -6,13 +6,8 @@ var getMessageButton = document.querySelector("#get-message")
 var meditateIcon = document.querySelector("#medIcon")
 var bottomBox = document.querySelector("#bottom-box")
 
-// var messageText = document.querySelector(".message-text")
-
 getMessageButton.addEventListener("click", getMessageType)
 
-// which radio button was checked?
-// run getRandomAff or getRandomMan to get a string
-// run displayMessage with that messageText
 function getMessageType() {
   var isMantraChecked = document.querySelector("#mantra-button").checked;
   var isAffirmChecked = document.querySelector("#affirmation-button").checked;
@@ -33,17 +28,5 @@ function displayMessage(showThisMessage) {
   meditateIcon.classList.add("hidden")
   bottomBox.classList.add("message-text")
   bottomBox.innerHTML = showThisMessage
+  // also put the white box back in?
 }
-
-// when Receive Message button is clicked, run getMessageType function
-// getMessageType looks at which radio button was checked,
-// and then runs either getRandomAff or getRandomMan function
-// which takes in the quote arrays and returns one of them as showThisMessage, a string,
-// then it runs displayMessage function with that Messages object
-// which hides the icon, adds message-text class to the bottom box, and
-// uses innerHTML to display
-
-
-
-
-// aldfj
---
+++ messages.js
@@ -0,0 +1,19 @@
+class Messages {
+  constructor() {
+    this.type = type
+  }
+}
+
+var affText = [
+  "I forgive myself and set myself free.",
+  "I believe I can be all that I want to be.",
+  "I am in the process of becoming the best version of myself.",
+  "I have the freedom and power to create the life I desire."
+]
+
+var mantraText = [
+  "I am enough.",
+  "Every day is a second chance.",
+  "Tell the truth and love everyone.",
+  "I am free from sadness."
+]
