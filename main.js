c2c315e8ab8568005d57baa879f2ff7cc400afdd
Ben Prat <bvprat@gmail.com>
2020-10-26 15:35:32 -0600

    Error Handling and Clear button functionality

--- main.js
+++ main.js
@@ -2,14 +2,19 @@ var selects = document.querySelectorAll('input[type="radio"]');
 var zen = document.querySelector('.zenIcon');
 var returnMessage = document.querySelector('.replace');
 var receiveBtn = document.querySelector('.receive-message-button');
+var clearBtn = document.querySelector('.clear-message-button');
 var messageContainer = document.querySelector('message-container');
 
-
-receiveBtn.addEventListener('click', showMessage)
-
+if (selects[0].checked === false || selects[1].checked === false) {
+    receiveBtn.disabled = true;
+}
 for (var i = 0; i < selects.length; i++) {
     selects[i].addEventListener('input', enableReceiveButton)
 }
+
+receiveBtn.addEventListener('click', showMessage)
+clearBtn.addEventListener('click', clearMessage)
+
 
 function getRandomIndex(array) {
     var randomIndex = Math.floor(Math.random() * array.length)
@@ -19,6 +24,7 @@ function getRandomIndex(array) {
 function showMessage() {
     var mantra = getRandomIndex(mantras)
     var affirmation = getRandomIndex(affirmations)
+
     if (selects[0].checked) {
         returnMessage.innerText = `${mantra}`
     } else if (selects[1].checked) {
@@ -32,9 +38,9 @@ function enableReceiveButton() {
     receiveBtn.removeAttribute('disabled')
 }
 
-function uncheckRadios() {
-    selects[0].checked = false
-    selects[1].checked = false
-}
+function clearMessage() {
+    returnMessage.innerText = ""
+    returnMessage.innerText = ""
+    zen.classList.remove('hidden')
 
-uncheckRadios()
+}
