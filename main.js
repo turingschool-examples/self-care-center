b1bc6e2583c7b1739aca2e58942ba6686568e952
Rachel Hendricks <>
2021-02-19 10:25:16 -0700

    Add mantras variable

---
+++ main.js
@@ -0,0 +1,17 @@
+var mantras = [
+  'Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.',
+  'Don’t let yesterday take up too much of today.',
+  'Every day is a second chance.',
+  'Tell the truth and love everyone.',
+  'I am free from sadness.',
+  'I am enough.',
+  'In the beginning it is you, in the middle it is you and in the end it is you.',
+  'I love myself.',
+  'I am present now.',
+  'Inhale the future, exhale the past.',
+  'This too shall pass.',
+  'Yesterday is not today.',
+  'The only constant is change.',
+  'Onward and upward.',
+  'I am the sky, the rest is weather.'
+];
