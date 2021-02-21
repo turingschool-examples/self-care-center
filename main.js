003ba73c87ea7319c34ea16a3448d94e5382bf8f
Ashish Malla <ashishmalla45@gmail.com>
2021-02-21 01:46:44 -0700

    Add favorite message feature

--- main.js
+++ main.js
@@ -6,21 +6,39 @@ var mantraLbl = document.querySelector('.mantra');
 var bellIcon = document.querySelector('.bell-icon');
 var showMsg = document.querySelector('.show-msg');
 
+var viewFavMsg = document.querySelector('.view-fav');
+
+var headingSection = document.querySelector('.heading');
+var messageSection  = document.querySelector('.message');
+var bellSection  = document.querySelector('.bell');
+var favSection  = document.querySelector('.fav-lists');
+
+
+
+
 var affirmations = [
   'I am love. I am purpose. I was made with divine intention.', 'I don’t sweat the small stuff.', 'I can. I will. End of story.', 'I am adventurous. I overcome fears by following my dreams.', 'I feed my spirit. I train my body. I focus my mind. It’s my time.', 'I am in charge of how I feel and today I am choosing happiness.', 'I will not compare myself to strangers on the Internet.', 'I am choosing and not waiting to be chosen.', 'I am enough.', 'I am whole.', 'I have the power to create change.', 'I let go of all that no longer serves me.', 'I refuse to give up because I haven’t tried all possible ways.'
 ];
 var mantras = [
-  'Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.', 'Don’t let yesterday take up too much of today.', 'Every day is a second chance.', 'Tell the truth and love everyone.', 'I am free from sadness.', 'I am enough.', 'In the beginning it is you, in the middle it is you and in the end it is you.', 'I love myself.', 'I am present now.', 'Inhale the future, exhale the past.', 'This too shall pass.', 'Yesterday is not today.', 'The only constant is change.', 'Onward and upward.', 'I am the sky, the rest is weather.'
+  'Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.','Don’t let yesterday take up too much of today.','Every day is a second chance.','Tell the truth and love everyone.','I am free from sadness.','I am enough.','In the beginning it is you, in the middle it is you and in the end it is you.','I love myself.','I am present now.','Inhale the future, exhale the past.','This too shall pass.','Yesterday is not today','The only constant is change.','Onward and upward.','I am the sky, the rest is weather.'
 ];
-var savedMsg = [];
+// var viewedMsg = [];
+var favSavedMsg = [];
 var currentMsg;
 
 var msgBtn = document.querySelector('.msg-btn');
 var clearBtn = document.querySelector('.clear-btn');
-
+var favBtn = document.querySelector('.fav-btn');
+var viewFavBtn = document.querySelector('.view-favMsg-btn');
+var goBackBtn = document.querySelector('.go-back');
 //event listeners go here 👇
 msgBtn.addEventListener('click', displayMsg);
 clearBtn.addEventListener('click', clearMsg);
+favBtn.addEventListener('click', favoriteMsg);
+favSection.addEventListener('click', deleteFavMsg);
+viewFavMsg.addEventListener('click',makeMiniFavMsgList);
+viewFavBtn.addEventListener('click', openFavList);
+goBackBtn.addEventListener('click', goToMainPg);
 //List of functions 👇
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
@@ -35,24 +53,86 @@ function clearPage(){
   mantraInput.checked = false;
   affirmInput.checked = false;
 }
+function clearMsg(){
+  showMsg.innerText = "";
+  favBtn.classList.add('hidden');
+  viewFavBtn.classList.add('hidden');
+  // msgBtn.disabled = false;
+  clearPage();
+}
 function getRandomAffirm(){
   showMsg.innerText = affirmations[getRandomIndex(affirmations)];
   hideIcon();
+  favBtn.innerText = "🤍";
+  currentMsg = showMsg.innerText;
 }
 function getRandomMantra(){
   showMsg.innerText = mantras[getRandomIndex(affirmations)];
   hideIcon();
+  favBtn.innerText = "🤍";
+  currentMsg = showMsg.innerText;
 }
 function displayMsg(){
   if(affirmInput.checked){
     getRandomAffirm();
+    favBtn.classList.remove('hidden');
+    viewFavBtn.classList.remove('hidden');
   }else if(mantraInput.checked){
     getRandomMantra();
+    favBtn.classList.remove('hidden');
+    viewFavBtn.classList.remove('hidden');
   }else{
     alert("🙏🏽 AT LEAST ONE MESSAGE OPTION MUST BE SELECTED 🙏🏽");
   }
 }
-function clearMsg(){
-  showMsg.innerText = "";
-  clearPage();
+
+
+function favoriteMsg(){
+  if(!favSavedMsg.includes(currentMsg)){
+    favBtn.innerText = "♥️";
+    // viewedMsg.push(currentMsg);
+
+    favSavedMsg.push(currentMsg);
+    makeMiniFavMsgList();
+
+  }else{
+    favBtn.innerText = "♥️";
+    alert("This message is already in your favorite list!");
+  }
+}
+// && !viewedMsg.includes(currentMsg)
+
+
+
+function makeMiniFavMsgList() {
+  viewFavMsg.innerHTML = '';
+  for (var i = 0; i < favSavedMsg.length; i++) {
+    viewFavMsg.innerHTML +=
+    `
+      <div class='new-fav-msg'>
+        <p>${favSavedMsg[i]}</p>
+        <button class='delete-fav-msg-btn'>Delete</button>
+      </div>
+    `
+  }
+}
+
+function deleteFavMsg(){
+  // var index = favSavedMsg.indexOf();
+  // if(index > -1){
+  //   favSavedMsg.splice(index, 1);
+  // }makeMiniFavMsgList();
+}
+
+function openFavList(){
+  favSection.classList.remove('hidden');
+  headingSection.classList.add('hidden');
+  messageSection.classList.add('hidden');
+  bellSection.classList.add('hidden');
+}
+function goToMainPg(){
+  favSection.classList.add('hidden');
+  headingSection.classList.remove('hidden');
+  messageSection.classList.remove('hidden');
+  bellSection.classList.remove('hidden');
 }
