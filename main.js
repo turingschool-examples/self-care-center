94a7ebe56e84c9edd73bf887be0995321ce8cd08
Megan McBride <megan.mcbride@colorado.edu>
2021-05-26 17:09:10 -0600

    Set button query selectors, include mantra and affirmation arrays, include failed function to impact radio buttons

---
+++ main.js
@@ -0,0 +1,56 @@
+// declared querySelectors
+// buttons
+var buttonAffirmation = document.getElementById('affirmation');
+var buttonMantra = document.getElementById('mantra');
+var buttonSubmit = document.querySelector('.submit');
+// global Variables
+var affirmations = [
+  'I forgive myself and set myself free.',
+  'I believe I can be all that I want to be.',
+  'I am in the process of becoming the best version of myself.',
+  'I have the freedom and power to create the life I desire.',
+  'I choose to be kind to myself and love myself unconditionally.',
+  'My possibilities are endless.',
+  'I am worthy of my dreams.',
+  'I am enough.',
+  'I deserve to be healthy and feel good.',
+  'I am full of energy and vitality and my mind is calm and peaceful.',
+  'Every day I am getting healthier and stronger.',
+  'I honor my body by trsuting the signals that it sends me.',
+  'I manifest perfect health by making smart choices.',
+]
+var mantras = [
+  'Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.',
+  'Do not let yesterday take up too much of today.',
+  'Evey day is a second chance.',
+  'Tell the truth and love everyone.',
+  'I am free from sadness.',
+  'I am enough.',
+  'In the beginning it is you, in the middle it is you, and in the end it is you.',
+  'I love myself.',
+  'I am present now.',
+  'Inhale the future, exhale the past.',
+  'This too shall pass.',
+  'Yesterday in not today.',
+  'The only constant is change.',
+  'Onward and upward.',
+  'I am the sky, the rest is weather.',
+]
+
+// eventListeners
+
+buttonSubmit.addEventListener('click', returnRandomMessage);
+// functions and handlers
+
+function returnRandomMessage() {
+  if (buttonAffirmation.checked) {
+    buttonMantra.checked === false;
+    getRandomMessage(affirmations);
+  } else if (buttonMantra.checked) {
+    buttonAffirmation.checked === false;
+    getRandomMessage(mantras);
+  }
+}
+
+function getRandomMessage(messages) {
+  return Math.floor(Math.random() * messages.length);
