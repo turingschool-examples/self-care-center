9e27a5cf11fb83a706a64f32d081db8591b40c96
Mae Duphorne <maelduphorne@gmail.com>
2021-05-27 12:44:41 -0600

    Make query selectors for message button functionality

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-
+// When a user selects a message option and then clicks the “Receive Message” button,
+//the user sees a random message from the list of possible messages for that category
+// When the message appears, the mediation icon disappears from the message area
+// need: access to
+    // radio button1, radio button 2, receive message button, meditation image,
+    // display messsage
+var receiveMessageBtn = document.querySelector(".message-button");
+var affirmationButton = document.querySelector(".radio-button1");
+var mantraButton = document.querySelector(".radio-button2");
+var image = document.querySelector(".meditation-image");
