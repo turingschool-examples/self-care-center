a85fdbd6c5bc0c70993fe7d151f24fd2f3ed24a5
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-05-27 14:28:29 -0600

    Add back to main button on showFavorites section

--- src/main.js
+++ src/main.js
@@ -6,11 +6,17 @@ var form = document.querySelector('form');
 var radio = form.elements['message-type'];
 var submit = document.getElementById("submit")
 var displayMessageSection = document.querySelector(".display-message");
+var homePageSection = document.querySelector(".main-page")
+var favoritesSection = document.querySelector(".favorites-page")
+var viewFavoriteMessagesButton = document.querySelector(".view-saved");
+var homeButton = document.querySelector(".home-button");
 
 
 
 /*---------Event Listeners ---------------*/
 submit.addEventListener("click", displayMessage);
+viewFavoriteMessagesButton.addEventListener("click", showFavorites);
+homeButton.addEventListener("click", showHomePage);
 
 
 
@@ -54,9 +60,34 @@ function favoriteAMessage() {
 function showFavorites() {
   homePageSection.classList.add('hidden');
   favoritesSection.classList.remove('hidden');
+
+  renderFavorites();
 }
 
-// Users should be able to navigate back to the main page by clicking a button.
+function showHomePage(){
+  homePageSection.classList.remove('hidden');
+  favoritesSection.classList.add('hidden');
+}
+
+renderFavorites(){
+  for (var i = 0; i < favoriteMessages.length; i++) {
+  //using +=
+  //add this html with this class each time.
+  //first think how do you want to set it up?
+
+  savedCoversSection.innerHTML +=
+  `
+  <section class="mini-cover" id="${savedCovers.length}">
+    <img class="cover-image" src=${currentCovers[i].cover}>
+    <h2 class="cover-title">${currentCovers[i].title}</h2>
+    <h3 class="tagline">A tale of <span class="tagline-1">${currentCovers[i].tagline1}</span> and <span class="tagline-2">${currentCover.tagline2}</span></h3>
+  </section>
+  `
+}
+
+}
+
+
 // Users should be able to remove a message from their list of favorites, by clicking a button.
 // As you add these new elements to the page, be sure to match the style of existing elements.
 
