7c57fb18be22cf221ecb3788df82caa62d3900b1
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-05-27 12:04:26 -0600

    Add Message class in message.js file

--- src/main.js
+++ src/main.js
@@ -7,8 +7,11 @@ var radio = form.elements['message-type'];
 var submit = document.getElementById("submit")
 var displayMessageSection = document.querySelector(".display-message");
 
+
+
 /*---------Event Listeners ---------------*/
 submit.addEventListener("click", displayMessage);
+
 
 
 /*---------Functions ---------------*/
@@ -26,15 +29,42 @@ function renderCurrentMessage(){
   displayMessageSection.innerHTML =
   `
   <p>${currentMessage}</p>
-  <button class="submit"><3</button>
+  <button class="favorite"><3</button>
   `
+
+
+  var favoriteButton = document.querySelector("button");
+  favoriteButton.addEventListener("click", favoriteAMessage);
+
 }
+
+
+
+
+function favoriteAMessage(e) {
+  favoriteButton = document.querySelector(".favorite");
+  var messageDisplayed = e.target.closest('p');
+  favoriteMessages.push(new Message(messageDisplayed));
+  console.log("messageDisplayed", messageDisplayed);
+  console.log("favoriteMessages", favoriteMessages);
+
+}
+
+
+
+//When the “Favorite” button is clicked, that message should be added to a new list of favorite messages.
+  //when the favorite button is clicked, add this current message to the array.
+    //put a query selector on the button within this method.
+
+      //querySelector(".favorite")
+    //have an event listner on that button
+
+
+    //when press favorite, create a new message object and push that in if id and message doesn't match.
+    //put id in each html random id with id = Math.random()
+    //
+
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
-
-
-  // if id.value === mantra, then pull from the affirmation  array and store that in a variable
-  // if id.value === affirmation, then pull from the mantra array
-  // store that in a currentMessage variable
