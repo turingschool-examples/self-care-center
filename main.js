b9bb050e38502899a67d6de8bed46b97f081d947
Rachel Jensen <rachelangela@gmail.com>
2021-05-28 15:39:28 -0600

    Set-up and pseudocode main.js file

--- main.js
+++ main.js
@@ -1,4 +1,61 @@
+var mantras = [
+  'Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.',
+  'Don’t let yesterday take up too much of today.',
+  'Every day is a second chance.',
+  'Tell the truth and love everyone.',
+  'I am free from sadness.',
+  'I am enough.',
+  'I love myself.',
+  'I am present now.',
+  'Inhale the future, exhale the past.',
+  'This too shall pass.',
+  'Yesterday is not today.',
+  'The only constant is change.',
+  'Love is a light that never dwelleth in a heart possessed by fear.',
+  'Onward and upward.',
+  'I am the sky, the rest is weather.',
+  'Where there is love, nothing is too much trouble, and there is always time.'
+];
+
+var affirmations = [
+  'I forgive myself and set myself free.',
+  'I believe I can be all that I want to be.',
+  'I am in the process of becoming the best version of myself.',
+  'I have the freedom & power to create the life I desire.',
+  'I choose to be kind to myself and love myself unconditionally.',
+  'My possibilities are endless.',
+  'I am worthy of my dreams.',
+  'I am enough.',
+  'I deserve to be healthy and feel good.',
+  'I am full of energy and vitality and my mind is calm and peaceful',
+  'Every day I am getting healthier and stronger.',
+  'I honor my body by trusting the signals that it sends me.',
+  'I manifest perfect health by making smart choices.'
+];
+
+var currentQuote = "";
+var savedQuotes = [];
+
 // query selectors
-var affirmationButton = document.getElementById('affirmation');
+var selectAffirmation = document.getElementById('affirmation');
+var selectMantra = document.getElementById('mantra');
+var receiveMessageBtn = document.getElementById('receive-message');
 
 // event listeners
+// when the Recieve Message button is clicked, it will display the Quote (function pseudocode below)
+receiveMessageBtn.addEventListener('click', displayQuote);
+
+//FUNCTIONS
+function displayQuote() {
+  // invoke getQuote() to make currentQuote the selected quote
+  // add currentQuote to message box element in <p>
+    // make message box element visible
+  // hide the meditator-img//
+}
+
+function getQuote() {
+  // if mantra/affirmation is selected
+    // generate a random number based on the the number mantras/affirmations available
+    // use random number to access a mantra/affirmations
+    // assign that quote as the value of current quote
+}
