fc7b349a0fdb334ec4b9d8c5e5a87dacae5ae9c6
John Kiernan <jkiernan12@yahoo.com>
2021-09-07 21:19:38 -0400

    Add query selectors

---
+++ main.js
@@ -0,0 +1,6 @@
+// Query selectors
+var messageButton = document.querySelector(".message-selection--button");
+var affirmationInput = document.querySelector("#affirmation-radio");
+var mantraInput = document.querySelector("#mantra-radio");
+var messageBox = document.querySelector(".quote");
+var messageIcon = document.querySelector(".quote--icon");
