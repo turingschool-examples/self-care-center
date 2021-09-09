1242f675b148b93275bf79487f41aa686f436317
Regan Losey <regan.losey@gmail.com>
2021-09-09 14:01:45 -0700

    Add mantras array and event listeners

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var affirmationBtn = document.getElementById('aff-btn');
 
 
 
+
 var affirmations =[
 "I forgive myself and set myself free.",
 
@@ -29,7 +30,41 @@ var affirmations =[
 
 "I honor my body by trusting the signals that it sends me.",
 
-"I manifest perfect health by making smart choices."]
+"I manifest perfect health by making smart choices."];
+var mantras = [
+  "Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.",
+
+"Don’t let yesterday take up too much of today.",
+
+"Every day is a second chance.",
+
+"Tell the truth and love everyone.",
+
+"I am free from sadness.",
+
+"I am enough.",
+
+"In the beginning it is you, in the middle it is you and in the end it is you.",
+
+"I love myself.",
+
+"I am present now.",
+
+"Inhale the future, exhale the past.",
+
+"This too shall pass.",
+
+"Yesterday is not today.",
+
+"The only constant is change.",
+
+"Onward and upward.",
+
+"I am the sky, the rest is weather."];
+
+
+
+
 
 
 
