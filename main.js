f2e288db0d0e87c81ef9160bfa5b394f2b616f5a
Kyra Bergsund <k.bergsund@gmail.com>
2021-09-10 21:34:33 -0700

    Add like button & view favorites page

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 // Query Selectors
-var receiveMessage = document.querySelector('#js-receive');
-var messageBox = document.querySelector('#js-message-box');
-var personIcon = document.querySelector('#js-person-icon');
-var message = document.querySelector('#js-message');
-var clearButton;
+var mainPage = document.querySelector('#js-main-page');
+var favMsgPage = document.querySelector('#js-fav-msg');
 var affirmationButton = document.querySelector('#affirmation');
 var mantraButton = document.querySelector('#mantra');
+var radioButtons = document.querySelector('form');
+var receiveMessageButton = document.querySelector('#js-receive');
+var messageDisplay = document.querySelector('#msg-and-fav');
+var personImage = document.querySelector('#js-person-img');
+var message = document.querySelector('#js-message');
+var showMainButton = document.querySelector('#back-to-main');
+var favMsgGrid = document.querySelector('#js-fav-msg-grid');
+var viewFavsButton;
+var likeButton;
+var clearButton;
 
 // Event Listeners
 affirmationButton.addEventListener('click', enableReceive);
 mantraButton.addEventListener('click', enableReceive);
+showMainButton.addEventListener('click', resetPage);
 
 // Global Variables
 var affirmations = [
@@ -44,39 +52,105 @@ var mantras = [
   'Onward and upward.',
   'I am the sky, the rest is weather.'
 ];
+favoritedMessages = [];
+var currentMessage;
 
 // Functions & Event Handlers
-function getRandomMessage(messageType) {
+function enableReceive() {
+  receiveMessageButton.addEventListener('click', displayMessage);
+}
+
+function changeHidden(hide, show) {
+  if (hide) {
+    for (var i = 0; i < hide.length; i++) {
+      hide[i].classList.add('hidden');
+    }
+  }
+  if (show) {
+    for (var i = 0; i < show.length; i++) {
+      show[i].classList.remove('hidden');
+    }
+  }
+}
+
+function getRandomIndex(messageType) {
   var index = Math.floor(Math.random() * messageType.length);
   return messageType[index];
 }
 
-function enableReceive() {
-  receiveMessage.addEventListener('click', displayMessage);
+function generateMessage() {
+  if (affirmationButton.checked) {
+    currentMessage = getRandomIndex(affirmations);
+  } else if (mantraButton.checked){
+    currentMessage = getRandomIndex(mantras);
+  }
+  return currentMessage;
 }
 
 function displayMessage() {
-  if (affirmationButton.checked) {
-    var randomMessage = getRandomMessage(affirmations);
-  } else if (mantraButton.checked){
-    var randomMessage = getRandomMessage(mantras);
+  message.innerText = generateMessage();
+  changeHidden([personImage], [messageDisplay]);
+  displayClearButton();
+  displayViewFavsButton();
+  enableFavorite();
+  likeButton.classList.remove('pink');
+  if (favoritedMessages.includes(currentMessage)) {
+    likeButton.classList.add('pink');
   }
-  personIcon.classList.add('hidden');
-  displayClear();
-  message.innerText = randomMessage;
 }
 
-function displayClear() {
+function displayClearButton() {
   clearButton = document.querySelector('#js-clear');
   clearButton.addEventListener('click', resetPage);
-  clearButton.classList.remove('hidden');
+  changeHidden([], [clearButton]);
+}
+
+function displayViewFavsButton() {
+  viewFavsButton = document.querySelector('#js-fav-button');
+  viewFavsButton.addEventListener('click', showFavPage);
+  changeHidden([], [viewFavsButton]);
+}
+
+function showFavPage() {
+  changeHidden([mainPage], [favMsgPage]);
+  makeFavMsgGrid();
+}
+
+function enableFavorite() {
+  likeButton = document.querySelector('#js-like');
+  likeButton.addEventListener('click', favoriteMessage);
 }
 
 function resetPage() {
-  receiveMessage.removeEventListener('click', displayMessage);
-  personIcon.classList.remove('hidden');
-  clearButton.classList.add('hidden');
-  message.innerText = '';
-  affirmationButton.checked = false;
-  mantraButton.checked = false;
+  receiveMessageButton.removeEventListener('click', displayMessage);
+  changeHidden([favMsgPage, messageDisplay, clearButton, viewFavsButton], [mainPage, personImage]);
+  for (var i = 0; i < radioButtons.elements.length; i++) {
+    radioButtons.elements[i].checked = false;
+  }
+}
+
+function favoriteMessage() {
+  if (!likeButton.classList.contains('pink')) {
+    likeButton.classList.add('pink');
+    if (!favoritedMessages.includes(currentMessage)) {
+      favoritedMessages.push(currentMessage);
+    }
+  } else {
+    likeButton.classList.remove('pink');
+    for (var i = 0; i < favoritedMessages.length; i++) {
+      if (favoritedMessages[i] === currentMessage) {
+        favoritedMessages.splice(i, 1);
+      }
+    }
+  }
+}
+
+function makeFavMsgGrid() {
+  favMsgGrid.innerHTML = '';
+  for (var i = 0; i < favoritedMessages.length; i++) {
+    favMsgGrid.innerHTML += `
+    <article class="mini-message">
+    <p>${favoritedMessages[i]}</p>
+    `
+  }
 }
