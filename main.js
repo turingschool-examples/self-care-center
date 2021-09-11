10d9c49bfa0edaea68a599964074c038a3855095
Regan Losey <regan.losey@gmail.com>
2021-09-11 12:30:43 -0700

    Consolidate affirmation and mantra functions

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var affirmationBtn = document.getElementById('aff-btn');
 var mantraBtn = document.getElementById('mantra-btn');
-var receiveMsgBtn = document.querySelector('.receive-msg-btn');
+var receiveMsgBtn = document.getElementById('receive-msg-btn');
 var selfCareImage = document.querySelector('.self-care-image');
 var messageBox = document.querySelector('.display-message');
 var deleteBtn = document.getElementById('delete-button');
@@ -69,22 +69,21 @@ var mantras = [
 
 window.addEventListener('load', displayWelcomePage);
 receiveMsgBtn.addEventListener('click', displayMessage);
-deleteBtn.addEventListener('click', deleteMessage);
+deleteBtn.addEventListener('click', function () {
+  hideDeleteBtn()
+  deleteMessage()
+})
+
+function hideDeleteBtn(){
+  deleteBtn.classList.add('hidden');
+}
 
 
 function displayMessage() {
   if (mantraBtn.checked) {
-    var randomMantra = mantras[getRandomIndex(mantras)];
-    selfCareImage.classList.add('hidden');
-    messageBox.classList.remove('hidden');
-    messageBox.innerText = `${randomMantra}`;
-    deleteBtn.classList.remove('hidden');
+    displayMantra()
   } else if (affirmationBtn.checked) {
-    var randomAffirmation = affirmations[getRandomIndex(affirmations)];
-    selfCareImage.classList.add('hidden');
-    messageBox.classList.remove('hidden');
-    messageBox.innerText = `${randomAffirmation}`;
-    deleteBtn.classList.remove('hidden');
+    displayAffirmation()
   }
 }
 
@@ -94,11 +93,31 @@ function displayWelcomePage(event) {
 }
 
 function deleteMessage() {
-  if (affirmationBtn.checked || mantraBtn.checked) {
+  if (messageBox.innerText){
+    var randomMantra = mantras[getRandomIndex(mantras)];
     alert("Message Deleted");
     messageBox.classList.add('hidden');
     selfCareImage.classList.remove('hidden');
+  } else if (!messageBox.innerText) {
+    var randomAffirmation = affirmations[getRandomIndex(affirmations)]
+    deleteBtn.classList.add('hidden');
   }
+}
+
+function displayMantra(){
+    var randomMantra = mantras[getRandomIndex(mantras)];
+    selfCareImage.classList.add('hidden');
+    messageBox.classList.remove('hidden');
+    messageBox.innerText = `${randomMantra}`;
+    deleteBtn.classList.remove('hidden');
+  }
+
+function displayAffirmation(){
+    var randomAffirmation = affirmations[getRandomIndex(affirmations)];
+    selfCareImage.classList.add('hidden');
+    messageBox.classList.remove('hidden');
+    messageBox.innerText = `${randomAffirmation}`;
+    deleteBtn.classList.remove('hidden');
 }
 
 function getRandomIndex(array) {
