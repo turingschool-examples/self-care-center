b724f924ed7f3d9c296fee0f12016611c9a78cb8
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2021-12-15 14:56:11 -0700

    Add query selectors and event listener

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-affirmations and mantras
+/// Query Selectors ///
+var msgButton = document.querySelector('.msg-button')
+var affirmation = document.getElementById('affirmation-select')
+var icon = document.querySelector('.icon')
+var mantra = document.getElementById('mantra-select')
+var display = document.querySelector('.display')
+
+/// Event Listeners ///
+msgButton.addEventListener('click', showMessage)
+
+/// Functionality ///
+
+function showMessage() {
+  display.innerText = ''
+  if(affirmation.checked) {
+    display.innerText = affirmations[getRandomIndex(affirmations)]
+  }
+  if(mantra.checked) {
+    display.innerText = mantras[getRandomIndex(mantras)]
+  }
+  hide(icon)
+};
+
+function hide(element) {
+element.classList.add('hidden')
+  }
+
+function show(element) {
+element.classList.remove('hidden')
+};
+
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+};
