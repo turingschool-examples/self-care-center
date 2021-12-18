21ea44880ca57aa08cc667d7057aee82aa69af83
Andrew Musselman <andrew.mussels@gmail.com>
2021-12-18 14:48:04 -0700

    Add query selectors for buttons and inputs

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var affirmations = ['I forgive myself and set myself free.',
 'I am full of energy and vitality and my mind is calm and peaceful.',
 'Every day I am getting healthier and stronger.',
 'I honor my body by trusting the signals that it sends me.',
-'I manifest perfect health by making smart choices.']
+'I manifest perfect health by making smart choices.'];
 
 var mantras = ['Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.',
 'Don’t let yesterday take up too much of today.',
@@ -16,4 +16,14 @@ var mantras = ['Breathing in, I send myself love. Breathing out, I send love to 
 'I am free from sadness.', 'I am enough.', 'In the beginning it is you, in the middle it is you and in the end it is you.',
 'I love myself.', 'I am present now.', 'Inhale the future, exhale the past.',
 'This too shall pass.', 'Yesterday is not today.', 'The only constant is change.',
-'Onward and upward.', 'I am the sky, the rest is weather.']
+'Onward and upward.', 'I am the sky, the rest is weather.'];
+
+var reveiveMessageButton = document.querySelector(#recieve-button);
+var radioButtonSelect = document.querySelectorAll('input[name="message-type"]');
+var shownMessageBox = document.querySelector('.shown-message-box')
+
+
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+};
