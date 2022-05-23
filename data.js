c05ba62b2b1d8bf95c8ae34d174c27698b1887e9
Shane Way <shane.c.way2@gmail.com>
2022-05-23 16:45:35 -0600

    This commit adds 20 elements to the affirmations array in data.js

--- data.js
+++ data.js
@@ -1,5 +1,30 @@
-var mantras = [];
+var affirmations = [
+  "I am in the right place at the right time, doing the right thing. - Louise Hay",
+  "Conscious breathing is my anchor. - Thich Nhat Hanh",
+  "You are loved just for being who you are, just for existing -Ram Dass.",
+  "The chance to love and be loved exists no matter where you are. - Oprah",
+  "Courage starts with showing up and letting ourselves be seen - Brené Brown",
+  "Make way for the unprecedented and watch your reality rearrange yourself. - Yrsa Daley-Ward",
+  "Open your heart and drink in the glorious day. - Heather Havrilesky",
+  "Am I good enough? Yes I am. - Michelle Obama",
+  "The perfect moment is this one - Jon Kabat-Zinn",
+  "I am deliberate and afraid of nothing. - Audre Lord",
+  "Your life is about to be incredible.",
+  "Who you are inside is what helps you make and do everything in life. - Fred Rogers",
+  "Your perspective is unique. It’s important and it counts. - Genn Close",
+  "Every day above earth is a good day. - Ernest Hemingway",
+  "Nothing can dim the light that shines from within. - Maya Angelou",
+  "You must do the things you think you cannot do. - Elenor Roosevelt",
+  "The secret of attraction is to love yourself. - Deepak Chopra",
+  "Good riddance to decisions that don't support self-care, self-value, and self-worth. - Oprah",
+  "I am the greatest. I said that even before I knew I was. - Muhammad li",
+  "I’m better than I used to be. Better than I was yesterday. But hopefully not as good as I’ll be tomorrow. - Marianne Williamson",
+ ];
 
 
 
-var affirmations = [];
+var mantras = [
+
+
+  
+];
