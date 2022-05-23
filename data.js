0f3c229f8acb8856f816779db091af41a41a7691
Shane Way <shane.c.way2@gmail.com>
2022-05-23 16:56:00 -0600

    This commit adds the mantra strings to the data.js file

--- data.js
+++ data.js
@@ -24,7 +24,20 @@ var affirmations = [
 
 
 var mantras = [
-
-
-  
+  "My mind is brilliant. My body is healthy. My spirit is tranquil.",
+  "I create my own path and walk it with joy.",
+  "My positive thoughts guide me to new heights."
+  "I am conquering my fears and becoming stronger each day.",
+  "I will have a good day, because it’s my choice.",
+  "I am not afraid to be wrong.”",
+  "My body is a temple. I keep my temple clean.",
+  "I hold the key to better health through eating better and exercise.”",
+  "Good health is a practice, and I have patience.",
+  "I listen to my body and give it what it needs.",
+  "Every cell in my body is alive and beautiful.",
+  "My potential is limitless, and I choose where to spend my energy.",
+  "While I support others, I also ask for help when needed.”",
+  "I surrender to the flow and have faith in the ultimate good.",
+  "I will speak with confidence and self-assurance.”",
+  "My commitment to myself is unbreakable.",
 ];
