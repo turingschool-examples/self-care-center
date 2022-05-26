1998de1acabc8f11fd73df0fcb99d8f45e804e64
Carissa Gross <carissamgross@gmail.com>
2022-05-26 07:34:13 -0500

    update html and javascript files

---
+++ main.js
@@ -0,0 +1,46 @@
+// Affirmations & Mantras
+var affirmations = ["I forgive myself and set myself free.",
+"I believe I can be all that I want to be.",
+"I am in the process of becoming the best version of myself.",
+"I have the freedom & power to create the life I desire.",
+"I choose to be kind to myself and love myself unconditionally.",
+"My possibilities are endless.",
+"I am worthy of my dreams.", "I am enough.",
+"I deserve to be healthy and feel good.",
+"I am full of energy and vitality and my mind is calm and peaceful.",
+"Every day I am getting healthier and stronger.",
+"I honor my body by trusting the signals that it sends me.",
+"I manifest perfect health by making smart choices."];
+
+var mantras =["Breathing in, I send myself love."
+"Breathing out, I send love to someone else who needs it.",
+"Don’t let yesterday take up too much of today.",
+"Every day is a second chance.",
+"Tell the truth and love everyone.",
+"I am free from sadness.",
+"I am enough.",
+"In the beginning it is you, in the middle it is you and in the end it is you.",
+"I love myself.",
+"I am present now.",
+"Inhale the future, exhale the past.",
+"This too shall pass.",
+"Yesterday is not today.",
+"The only constant is change.",
+"Be impeccable with your word. Don't take anything personally. Don't make assumptions. Always do your best.",
+"Onward and upward.",
+"I am the sky, the rest is weather."]
+
+// Event Listeners - Functions - Other Code
+var receiveMessage = document.querySelector('.receive_message')
+var affirmation = document.getElementById('affirmation');
+var mantra = document.getElementById('mantra');
+
+ button.addEventListener('click', mantraMessage)
+ function mantraMessage
+
+
+ function getRandomIndex(array) {
+   return Math.floor(Math.random() * array.length);
+ };
+// radio button needs to return the selected value
+// selected value returns a message when receive message is clicked
