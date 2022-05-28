73a3b155e7dd0b528ecd1e7f70774e02c58721fd
Shane Way <shane.c.way2@gmail.com>
2022-05-28 13:37:51 -0600

    This commits makes the affirmation and mantra button functions select random messages from their respective arrays

---
+++ main.js
@@ -0,0 +1,75 @@
+class Message {
+  constructor(mantra, affirmation) {
+    this.mantra = mantra;
+    this.affirmation = affirmation;
+  }
+}
+
+//Global variable
+// var currentMessage;
+
+
+//Arrays
+var affirmations = [
+  "I am in the right place at the right time, doing the right thing. - Louise Hay",
+  "Conscious breathing is my anchor. - Thich Nhat Hanh",
+  "You are loved just for being who you are, just for existing -Ram Dass.",
+  "The chance to love and be loved exists no matter where you are. - Oprah",
+  "Courage starts with showing up and letting ourselves be seen - Brené Brown",
+  "Make way for the unprecedented and watch your reality rearrange yourself. - Yrsa Daley-Ward",
+  "Open your heart and drink in the glorious day. - Heather Havrilesky",
+  "Am I good enough? Yes I am. - Michelle Obama",
+  "The perfect moment is this one - Jon Kabat-Zinn",
+  "I am deliberate and afraid of nothing. - Audre Lord",
+  "Your life is about to be incredible.",
+  "Who you are inside is what helps you make and do everything in life. - Fred Rogers",
+  "Your perspective is unique. It’s important and it counts. - Genn Close",
+  "Every day above earth is a good day. - Ernest Hemingway",
+  "Nothing can dim the light that shines from within. - Maya Angelou",
+  "You must do the things you think you cannot do. - Elenor Roosevelt",
+  "The secret of attraction is to love yourself. - Deepak Chopra",
+  "Good riddance to decisions that don't support self-care, self-value, and self-worth. - Oprah",
+  "I am the greatest. I said that even before I knew I was. - Muhammad li",
+  "I’m better than I used to be. Better than I was yesterday. But hopefully not as good as I’ll be tomorrow. - Marianne Williamson",
+ ];
+var mantras = [
+   "My mind is brilliant. My body is healthy. My spirit is tranquil.",
+   "I create my own path and walk it with joy.",
+   "My positive thoughts guide me to new heights.",
+   "I am conquering my fears and becoming stronger each day.",
+   "I will have a good day, because it’s my choice.",
+   "I am not afraid to be wrong.",
+   "My body is a temple. I keep my temple clean.",
+   "I hold the key to better health through eating better and exercise.",
+   "Good health is a practice, and I have patience.",
+   "I listen to my body and give it what it needs.",
+   "Every cell in my body is alive and beautiful.",
+   "My potential is limitless, and I choose where to spend my energy.",
+   "While I support others, I also ask for help when needed.",
+   "I surrender to the flow and have faith in the ultimate good.",
+   "I will speak with confidence and self-assurance.",
+   "My commitment to myself is unbreakable.",
+ ];
+
+//variables to query buttons
+var mantraButton = document.querySelector("#mantra-button");
+var affirmationButton = document.querySelector("#affirmation-button");
+var recieveButton = document.querySelector(".recieve-button");
+
+//Recieve messgage event listener
+recieveButton.addEventListener('click', displayRandomMessage);
+
+/*~~~~~~~~~~~~~~~~~ITERATION 1 ~~~~~~~~~~~~~~~~~~~~~~*/
+
+// Global function for randomly indexing goodVibesArrays
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+function displayRandomMessage() {
+  if (mantraButton.checked === true && affirmationButton.checked === false) {
+    return mantras[getRandomIndex(mantras)];
+  } else if (affirmationButton.checked === true && mantraButton.checked === false) {
+    return affirmations[getRandomIndex(affirmations)];
+    }
+  }
