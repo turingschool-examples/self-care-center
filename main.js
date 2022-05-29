1acae7b00e2510c1e8073f75bc2d0b412a455df6
Maya Kappen <mayakappen@gmail.com>
2022-05-29 16:24:59 -0600

    add more form stuff

--- main.js
+++ main.js
@@ -2,18 +2,25 @@
 /// Query Selectors ///
 var messageButton = document.getElementById("RM");
 
+var createButton = document.getElementById("CM");
+
 var quoteOutput = document.getElementById("quote-output");
 
 var whatButton = document.getElementsByClassName("what-button");
+
+var makeOwn = document.querySelector('.make-own')
 
 /// Event Listeners ///
 
 messageButton.addEventListener('click', displayQuote);
 
+createButton.addEventListener('click', newQuote);
+
 /// Functions ///
 
 function displayQuote() {
   document.getElementById("tacobell").style.visibility = "hidden";
+  makeOwn.classList.add('hidden');
   if (document.getElementById("rb1").checked) {
      return quoteOutput.innerText = affirmations[Math.floor(Math.random() * affirmations.length)];
 }
@@ -21,5 +28,8 @@ else if (document.getElementById("rb2").checked) {
     return quoteOutput.innerText = mantras[Math.floor(Math.random() * mantras.length)];
   }
 }
-
-///function newQuote()
+function newQuote() {
+  document.getElementById("tacobell").style.visibility = "hidden";
+  makeOwn.classList.remove('hidden');
+  quoteOutput.innerText = "";
+}
