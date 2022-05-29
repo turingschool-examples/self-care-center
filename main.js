f358860cbd2b4cc82615cc418a16cd7dcc71c3f8
Carissa Gross <carissamgross@gmail.com>
2022-05-29 10:26:11 -0500

    update receive message button to generate random message and add clear message button

--- main.js
+++ main.js
@@ -1,54 +1,12 @@
 var messageType = document.querySelectorAll('input[name="messageType"]');
-var receiveMessage = document.querySelector('.receive_message');
+var receiveMessageButton = document.querySelector('.receive_message');
 var meditateImage = document.querySelector("img[src='./assets/meditate.svg']");
-
-
-// returns the value of selected radio buttons
-function isChecked() {
-  for (var i = 0; i < messageType.length; i++) {
-    if (messageType[i].checked) {
-      return messageType[i].value
-    }
-  }
-}
-
-// this function will check to see if the mantra button was checked using an if statement to confirm the value
-function returnMantra() {
-  var mantraButton= isChecked();
-    if (mantraButton === 'affirmation') {
-      return [mantra[1]]; //this was to see if this was all working properly - funcation returned mantra in the 1 index of the array on the console
-
-    }
-}
-
-
-// this function will check to see if the affirmation button was checked using an if statement to confirm the value
-function returnAffirmation() {
-  var affirmationButton = isChecked();
-    if (affirmationButton === 'affirmation') {
-      return [affirmations[1]];
-
-    }
-}
-
-
-document.addEventListener('submit', receiveMessage)
+var imageBox = document.querySelector('.imageBox');
+var clearButton = document.querySelector('.clear_message');
+var message = document.querySelector('.display-message');
 
 
 
-// selected value returns a message when receive message is clicked
-// receive message = submit
-// image disappears to display message
-
-function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
-};
-
-
-
-
-
-// Affirmations & Mantras
 var affirmations =
 ["I forgive myself and set myself free.",
 "I believe I can be all that I want to be.",
@@ -81,3 +39,43 @@ var mantras =
 "Be impeccable with your word. Don't take anything personally. Don't make assumptions. Always do your best.",
 "Onward and upward.",
 "I am the sky, the rest is weather."]
+
+// function for random index
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+affirmations[getRandomIndex(affirmations)];
+mantras[getRandomIndex(mantras)];
+
+// returns the value of selected radio buttons
+function isChecked() {
+  for (var i = 0; i < messageType.length; i++) {
+    if (messageType[i].checked) {
+      return messageType[i].value
+    }
+  }
+}
+
+receiveMessageButton.addEventListener('click', displayMessage)
+
+function displayMessage(event) {
+  var checkedButton = isChecked();
+    if (checkedButton === 'affirmation') {
+        meditateImage.classList.add('hidden');
+        return imageBox.innerHTML +=
+        `<section class="display-message">${affirmations[getRandomIndex(affirmations)]}</section>`
+      } else if (checkedButton === 'mantra') {
+          meditateImage.classList.add('hidden');
+          return imageBox.innerHTML +=
+          `<section class="display-message">${mantras[getRandomIndex(mantras)]}</section>`
+        } else {
+          return imageBox.innerHTML = `Please pick an option`
+          }
+}
+
+clearButton.addEventListener('click', clearMessage)
+
+function clearMessage() {
+// return meditateImage.classList.remove('hidden');
+}
