0ef7b8ce30d03b02ac7648c8ac2caa0109580a60
Maia Cochran <maia.cochran1@gmail.com>
2022-05-29 13:54:02 -0700

    Add javascript functionality

---
+++ main.js
@@ -0,0 +1,79 @@
+//messages
+var mantras = [
+'Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.',
+'Do not let yesterday take up too much of today.',
+'Every day is a second chance.',
+'Tell the truth and love everyone.',
+'I am free from sadness.',
+'I am enough.',
+'In the beginning it is you, in the middle it is you and in the end it is you.',
+'I love myself.',
+'I am present now.',
+'Inhale the future, exhale the past.',
+'This too shall pass.'
+];
+
+var affirmations = [
+'I forgive myself and set myself free.',
+'I believe I can be all that I want to be.',
+'I am in the process of becoming the best version of myself.',
+'I have the freedom & power to create the life I desire.',
+'I choose to be kind to myself and love myself unconditionally.',
+'My possibilities are endless.',
+'I am worthy of my dreams.',
+'I am enough.',
+'I deserve to be healthy and feel good.',
+'I am full of energy and vitality and my mind is calm and peaceful.',
+'Every day I am getting healthier and stronger.',
+'I honor my body by trusting the signals that it sends me.',
+'I manifest perfect health by making smart choices.'
+];
+
+//query selectors
+var receiveMessage = document.querySelector('.receive-message-button');
+var radioButtons = document.querySelector('.message-buttons');
+var meditationGuy = document.querySelector('img');
+// var meditationMessage = document.querySelector('.guy-container');
+var message = document.querySelector('.message');
+var messageContainer = document.querySelector('.message-container');
+// var radio = document.querySelector("input[type='radio']:checked");
+
+//event listeners
+receiveMessage.addEventListener('click', displayMessage);
+
+//functions
+function displayMessage(event){
+  event.preventDefault();
+  hideMeditationGuy();
+  showMessage();
+};
+
+function hideMeditationGuy(){
+  meditationGuy.classList.add('hidden');
+  messageContainer.classList.remove('hidden');
+};
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+};
+
+function showMessage(){
+var radio = document.querySelector("input[type=radio]:checked");
+var randomAffirmation = getRandomIndex(affirmations);
+var randomMantra = getRandomIndex(mantras);
+  if(radio.value === "affirmations"){
+    message.innerHTML = `<section class"message-type"> ${affirmations[randomAffirmation]} </section>`;
+  } else {
+    message.innerHTML = `<section class"message-type"> ${mantras[randomMantra]} </section>`;
+  }
+};
+
+
+
+
+
+
+//query selctor is the same,
+//event listener will be similar,
+//the function of the event listener will be different
+//between affirmation and mantra result
