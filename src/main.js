90804fe5c6e435c38b0ddecff5d9c5d48e345b74
Chantal Goethals <chantalgoethals@gmail.com>
2022-05-30 00:55:52 -0600

    Fix: add rest message to page

--- src/main.js
+++ src/main.js
@@ -6,23 +6,14 @@
 var clearBtn = document.querySelector('.clear-btn');
 var manifestBtn = document.querySelector('.manf-btn');
 var hideSymbol = document.querySelector('.symbol-1-b');
-<<<<<<< HEAD
 var welcomeBox = document.querySelector('cb-1');
 var showMessage = document.querySelector('.cb-2');
 var resetBox = document.querySelector('.cb-3');
 var message = document.querySelector('.message');
 var radMa = document.querySelector('#mantra')
 var radAf = document.querySelector('#affirmation')
-=======
-var welcomeBox = document.querySelector('#cb-1');
-var showMessage = document.querySelector('#cb-2');
-var showMessage2 = document.querySelector('#cb-3');
-var message = document.querySelector('message');
-var radMa = document.querySelector('.affirmation')
-var radAf = document.querySelector('.mantra')
->>>>>>> main
 var radios = document.querySelector('.box-2')
-var resetMsg = document.querySelector('.flip-box')
+var chngMsg = document.querySelector('.flip-box')
 
 
 //---> EVENT LISTENERS
@@ -30,7 +21,6 @@ manifestBtn.addEventListener('click', viewMessage)
 clearBtn.addEventListener('click', clearScreen)
 
 
-<<<<<<< HEAD
 //---> EVENT HANDLERS
 
 
@@ -45,20 +35,27 @@ function viewMessage(){
  hideSymbol.classList.add('hidden');
  clearBtn.classList.remove('hidden');
  showMessage.classList.remove('hidden');
- resetBox.classList.add('hidden');
+ resetBox.classList.remove('hidden');
  message.classList.remove('hidden');
 }
 
 
-function clearScreen() {
+function resetMsg() {
   hideSymbol.classList.remove('hidden');
   message.classList.add('hidden');
   clearBtn.classList.add('hidden');
   showMessage.classList.add('hidden');
-  resetBox.classList.add('hidden');
-
+  resetBox.classList.remove('hidden');
 }
 
+function clearScreen(){
+  if(message.innerText = ''){
+    document.querySelector('chakra-box.cb-3').reset();
+  }
+  hideSymbol.classList.remove('hidden');
+  clearBtn.classList.add('hidden');
+  resetBox.classList.remove('hidden');
+}
 
 
 function randomizeAf(){
@@ -72,47 +69,3 @@ function randomizeMa(){
   message.innerText = randM
 
 };
-=======
-
-
-//---> EVENT HANDLERS
-
-function viewMessage(){
-  event.preventDefault()
-  hideSymbol.classList.add("hidden")
- if (radAf === radios.onclick){
-   return randomizeAf() add;
- } else if (radMa === radios.onclick) {
-   return randomizeMa();
- }else {
-   alert(`Positivity is just one choice below`)
-
- }
-}
-
-function clearScreen() {
-  hideSymbol.classList.add('hidden');
-  showMessage.classList.remove('hidden');
-  clearBtn.classList.remove('hidden');
-}
-// welcomeBox.classList.add("hidden")
-
-function randomizeAf(){
-  var randA = affirmations[Math.floor(Math.random() * affirmations.length)];
-  showMessage.innerText = randA
-};
-
-function randomizeMa(){
-  var randM = affirmations[Math.floor(Math.random() * mantras.length)];
-  showMessage.innerText = randM
-};
-
-function rstMsg(){
-  var messages = viewMessage()
-  if(welcomeBox.innerText = messages){
-    document.querySelector('message').reset();
-  }
-  hideSymbol.classList.remove('hidden');
-  clearBtn.classList.add('hidden');
-}
->>>>>>> main
