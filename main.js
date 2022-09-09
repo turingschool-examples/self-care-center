04019f90c776793be546a1e7f275c1e4dd578eb3
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-09-09 16:34:55 -0700

    format, create, and display paragraph element of a quote when user clicks recieve message button

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 var radioButtons = document.querySelectorAll('.radio-buttons');
 var recieveButton = document.querySelector('#recieve-message-button');
 var image = document.querySelector('.meditate-icon');
+var outputMessage = document.querySelector('p');
 
-recieveButton.addEventListener('click', removeImage);
+recieveButton.addEventListener('click', updateMessageBox);
 
 var messageType;
+var affirmationList = ["I am free", "I am good", "I am smart"];
 
 function checkMessageType(){
-  for(var i = 0;i<radioButtons.length;i++)
-  {
-    if(radioButtons[i].checked===true)
-    {
+  for(var i = 0;i<radioButtons.length;i++){
+    if(radioButtons[i].checked===true){
       messageType = radioButtons[i].value;
       break;
     }
@@ -18,6 +18,7 @@ function checkMessageType(){
   console.log(`You selected ${messageType}`);
 }
 
-function removeImage(){
+function updateMessageBox(){
   image.className += " hidden";
+  outputMessage.innerText = affirmationList[0];
 }
