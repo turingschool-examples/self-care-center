bec1303691138106880cd22991197ae928eace9e
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-09-09 16:07:35 -0700

    create checkMessageType function to determine result of radio buttons

--- main.js
+++ main.js
@@ -2,3 +2,17 @@ var radioButtons = document.querySelectorAll('.radio-buttons');
 var recieveButton = document.querySelector('#recieve-message-button');
 
 recieveButton.addEventListener('click', checkMessageType);
+
+var messageType;
+
+function checkMessageType(){
+  for(var i = 0;i<radioButtons.length;i++)
+  {
+    if(radioButtons[i].checked===true)
+    {
+      messageType = radioButtons[i].value;
+      break;
+    }
+  }
+  console.log(`You selected ${messageType}`);
+}
