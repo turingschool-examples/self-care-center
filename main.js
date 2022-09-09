15c7548ca1f8cd92f431d11d064018814eb1109d
Angela Wirth <angiewirth92@gmail.com>
2022-09-08 20:01:15 -0400

    add querySelectors, eventListener and arrays

---
+++ main.js
@@ -0,0 +1,19 @@
+//querySelectors
+submitButton = document.querySelector('.submit')
+printMessage = document.querySelector('.print')
+
+//eventListeners
+submitButton.addEventListener('click',)
+
+//arrays
+affirmations = []
+
+mantras = []
+
+//functions
+
+function displayMessage() {
+
+}
+
+
