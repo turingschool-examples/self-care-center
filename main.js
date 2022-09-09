accd19ea0964c74d4dc133844968a7fa16bf87b9
Angela Wirth <angiewirth92@gmail.com>
2022-09-08 20:34:07 -0400

    trying out displays

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 //querySelectors
-submitButton = document.querySelector('.submit')
+submitButton = document.querySelector('button')
 printMessage = document.querySelector('.print')
-
+svg = document.querySelector('svg')
 //eventListeners
-submitButton.addEventListener('click',)
+submitButton.addEventListener('click', displayMessage)
 
 //arrays
 affirmations = []
@@ -13,7 +13,15 @@ mantras = []
 //functions
 
 function displayMessage() {
+  for(var i = 0; i < affirmations.length; i++)
+  printMessage.innerHTML += `${affirmations[i]}`
 
+  for(var i = 0; i < mantras.length; i++)
+  printMessage.innerHTML += `${mantras[i]}`
+}
+
+function hideSvg() {
+  svg.classList.add('hidden')
 }
 
 
