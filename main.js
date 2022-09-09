f9d2159c945a1656b788d271cbc58a51519667dd
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-09-09 16:06:17 -0700

    create element variables and one event listener

---
+++ main.js
@@ -0,0 +1,4 @@
+var radioButtons = document.querySelectorAll('.radio-buttons');
+var recieveButton = document.querySelector('#recieve-message-button');
+
+recieveButton.addEventListener('click', checkMessageType);
