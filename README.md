4632745eb33d3ffb75aefc5323f1a5f0bb299270
Tim T <tim.email.personal@gmail.com>
2022-09-10 09:28:54 -1000

    Refactor all code and complete questions in readme

--- README.md
+++ README.md
@@ -30,21 +30,28 @@ ______________________________________________________
 
 ### Abstract:
 [//]: <> (Briefly describe what you built and its features. What problem is the app solving? How does this application solve that problem?)
-1. I built a desktop app
+- I built a desktop application that allows the user to receive randomly either a mantra or affirmation message. Ther user can favorite mantras and affirmations, go to a favorites view, and create their own message that will be added to the favorites view as well as the mantra/affirmation lists. 
+- This application solves the problem of wanting a quick and easy way to receive a self care message. It solves this problem by providing a list of existing affirmations and mantras and allowing the user to simply click through these messages as well as favorite them into a list. 
 ### Installation Instructions:
 [//]: <> (What steps does a person have to take to get your app cloned down and running?)
-
+- They will need the link to the gitHub repository. Then, they can clone to their local machine following the instructions on the github site. Once the repo is cloned, they can use the terminal to enter the appropriate directory and run <open> to view the application in their browser. 
 ### Preview of App:
 [//]: <> (Provide ONE gif or screenshot of your application - choose the "coolest" piece of functionality to show off.)
 
 ### Context:
 [//]: <> (Give some context for the project here. How long did you have to work on it? How far into the Turing program are you?)
-
+- I was able to complete this project in around 6 hours during the third week of Turing mod1. 
 ### Contributors:
 [//]: <> (Who worked on this application? Link to their GitHubs.)
-
+- I was the sole contributer. 
 ### Learning Goals:
 [//]: <> (What were the learning goals of this project? What tech did you work with?)
-
+- Gain experience building an application that utilizes HTML, CSS and JavaScript
+- Write HTML and CSS to match a provided comp
+- Understand how to listen to and respond to user events
+- Individualize your programming skill set
 ### Wins + Challenges:
 [//]: <> (What are 2-3 wins you have from this project? What were some challenges you faced - and how did you get over them?)
+- I was able to go beyond the MVP requirements and completed both the error handling and favoriting fast follow features. 
+- I was proud of my ability to spot and debug the application so the user experience was seamless. 
+- I learned a lot about flexbox through self study which I applied to this project. 
--- main.js
+++ main.js
@@ -1,53 +1,63 @@
+//See variables below
 var mantrasArray = ["Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.", "Don’t let yesterday take up too much of today.", "Every day is a second chance."]
 var affirmationsArray = ["I forgive myself and set myself free.", "I believe I can be all that I want to be.", "I am in the process of becoming the best version of myself."]
+var favoritesArray = []
 var receiveMessageButton = document.querySelector("button")
 var mantraButton = document.getElementById("mantraLabel")
 var affirmationButton = document.getElementById("affirmationLabel")
 var clearButton = document.querySelector(".clearButton")
 var favoriteButton = document.querySelector(".favoriteButton")
+var viewFavoritesButton = document.querySelector("#viewFavoritesButton")
+var flexboxContainer = document.querySelector(".flexbox-container")
+var h2Header = document.querySelector("h2")
+var homeButton = document.querySelector(".homeButton")
+var clearButton2 = document.querySelector(".clearButton2")
+var favoritesArrayView = document.querySelector(".favoritesArrayView")
+var containerFavoritesPage = document.querySelector(".containerFavoritesPageView")
+var favoriteButton = document.querySelector(".favoriteButton")
+viewFavoritesButton.addEventListener('click', goToFavoritesView)
+var meditationImageHTML = document.querySelector('.meditationImage')
+var flexBox2 = document.querySelector('.flexbox-item-2')
+var flexContainerButtons = document.querySelector("#flex-container-buttons")
+var flexBox2PhraseContainer = document.querySelector("#flexbox-item-2-phrase-container")
+var addButton = document.querySelector(".addButton")
 
+//See default classes below
 clearButton.classList.remove('hidden')
 favoriteButton.classList.remove('hidden')
 
+//See event listeners below
+addButton.addEventListener('click', saveInput) 
 affirmationButton.addEventListener('click', selectAffirmation)
+mantraButton.addEventListener('click', selectMantra)
+receiveMessageButton.addEventListener('click', receiveMessage)
+homeButton.addEventListener('click', goToHomePage)
+clearButton2.addEventListener('click', clearFavoritesArray)
 
+//See functions below 
 function selectAffirmation() {
     mantraButton.checked = false
     document.querySelector('#receiveMessageButtonId').disabled = false;
 }
-
-mantraButton.addEventListener('click', selectMantra)
-
 function selectMantra() {
     affirmationButton.checked = false
     document.querySelector('#receiveMessageButtonId').disabled = false;
 }
-
-receiveMessageButton.addEventListener('click', receiveMessage)
-
-var meditationImageHTML = document.querySelector('.meditationImage')
-var flexBox2 = document.querySelector('.flexbox-item-2')
-
 function getRandomNumber() {
-    //refactor to array.length to make dynamic 
-    return Math.floor(Math.random() * 3)
+    if(affirmationButton.checked) {
+    return Math.floor(Math.random() * affirmationsArray.length)
+    } else if (mantraButton.checked) {
+        return Math.floor(Math.random() * mantrasArray.length)
+    }
 }
-
-var flexContainerButtons = document.querySelector("#flex-container-buttons")
-var flexBox2PhraseContainer = document.querySelector("#flexbox-item-2-phrase-container")
-
 function receiveMessage() {
     if (!mantraButton.checked && !affirmationButton.checked) {
         document.querySelector('#receiveMessageButtonId').disabled = true;
         alert("Please select affirmation or mantra and click receive message again.")
-        //add opacity to receive message button when disabled
         } else if (mantraButton.checked) {
             document.querySelector('#receiveMessageButtonId').disabled = false;
             flexBox2.innerText = ""
             flexBox2.innerText += mantrasArray[getRandomNumber()]
-            // clearButton.classList.remove('hidden')
-            // favoriteButton.classList.remove('hidden')
-            // flexBox2.innerHTML += `<button id="clearButton">Clear</button><button id="favoriteButton">💛</button>`
             var clearButton = document.querySelector(".clearButton")
             clearButton.addEventListener('click', clearMessage)
             var favoriteButton = document.querySelector(".favoriteButton")
@@ -56,56 +66,23 @@ function receiveMessage() {
                 document.querySelector('#receiveMessageButtonId').disabled = false;
                 flexBox2.innerText = ""
                 flexBox2.innerText += affirmationsArray[getRandomNumber()]
-                // flexBox2.innerHTML += `
-                // <button id="clearButton">Clear</button>
-                // <button id="favoriteButton">💛</button>`
                 var clearButton = document.querySelector(".clearButton")
                 clearButton.addEventListener('click', clearMessage)
                 var favoriteButton = document.querySelector(".favoriteButton")
                 favoriteButton.addEventListener('click', addToFavorites)
     }
 }
-// var clearButton = document.querySelector("#clearButton")
-// var clearButton = document.getElementById("clearButton")
-
 function clearMessage() {
     flexBox2.innerText = ""
     flexBox2.innerHTML += `<img class="meditationImage" src="./assets/meditate.svg">`
     mantraButton.checked = false
     affirmationButton.checked = false
 }
-
-
 function addToFavorites() {
     favoritesArray.push(flexBox2.innerText)
     alert("Added to favorites!")
-   //push to array
-   //confirmation alert 
 }
-var viewFavoritesButton = document.querySelector("#viewFavoritesButton")
-var flexboxContainer = document.querySelector(".flexbox-container")
-var h2Header = document.querySelector("h2")
-var homeButton = document.querySelector(".homeButton")
-var clearButton2 = document.querySelector(".clearButton2")
-
-
-
-
-
-var favoritesArrayView = document.querySelector(".favoritesArrayView")
-var containerFavoritesPage = document.querySelector(".containerFavoritesPageView")
-
-// favoritesArrayView.classList.add('hidden')
-// containerFavoritesPage.classList.add('hidden')
-var favoriteButton = document.querySelector(".favoriteButton")
-
-
-var favoritesArray = []
-
-viewFavoritesButton.addEventListener('click', goToFavoritesView)
-
 function goToFavoritesView() {
-    //hides other pages
     favoritesArrayView.innerHTML = ""
     flexboxContainer.classList.add("hidden")
     containerFavoritesPage.classList.remove('hidden')
@@ -114,21 +91,12 @@ function goToFavoritesView() {
     clearButton2.classList.remove('hidden')
     favoriteButton.classList.remove('hidden')
     h2Header.innerText = "Favorites"
-    //unhide two new buttons home and clear favorites 
-    //changes inner text of h2 to favorites 
-    //shows a new div 
-    //for loop through array adds sparkle emoji
-
+    var userInput = document.querySelector('.inputField')
+    userInput.value = ""
     for (var i = 0; i < favoritesArray.length; i++) {
         favoritesArrayView.innerHTML += `<div>✨${favoritesArray[i]}✨</div>`
     }
-    // favoritesArrayView.innerText = "test"
-    //for loop inserts inner text into new div
-    //add listening event to both home and clear button to fire a different function
 }
-
-homeButton.addEventListener('click', goToHomePage)
-
 function goToHomePage() {
     flexboxContainer.classList.remove("hidden")
     containerFavoritesPage.classList.add('hidden')
@@ -138,10 +106,20 @@ function goToHomePage() {
     favoriteButton.classList.remove('hidden')
     h2Header.innerText = "Which type of message?"
 }
-
-clearButton2.addEventListener('click', clearFavoritesArray)
-
 function clearFavoritesArray() {
     favoritesArrayView.innerHTML = ""
     favoritesArray = []
+}
+function saveInput() {
+    var userInput = document.querySelector('.inputField').value
+    if (!userInput) {
+        alert("Please enter text and click add.")
+    } else {
+        userInput = document.querySelector('.inputField').value
+        favoritesArray.push(userInput)
+        mantrasArray.push(userInput)
+        affirmationsArray.push(userInput)
+        alert("Added!")
+    }
+    goToFavoritesView()
 }
