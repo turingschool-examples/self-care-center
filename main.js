d4a452846abb58bc0107a9652244499070b97976
Daniel Curtin <dcurtin.work@gmail.com>
2022-12-14 14:11:27 -0700

    Refactor radio buttons and display random messages

--- main.js
+++ main.js
@@ -1,21 +1,32 @@
 var affirmButton = document.querySelector("#affirmation");
 var mantraButton = document.querySelector("#mantra");
-var submitButton = document.querySelector("#receiv-msg");
+var submitButton = document.querySelector("#receive-msg");
+var medImg = document.querySelector(".meditation-icon");
 var displayedMessage = document.querySelector(".displayed-message");
 
 //event listeners
-affirmButton.addEventListener("click", clearMantra);
-mantraButton.addEventListener("click", clearAffirm);
+submitButton.addEventListener("click", outputRandom);
 
 //functions
 function getRandomIndex(array) {
-    return Math.floor(Math.random(), array);
+    return Math.floor(Math.random() * array.length);
 };
 
-function clearMantra() {
-    mantraButton.checked = false;
+function displayMessage() {
+    medImg.classList.add("hidden");
+    displayedMessage.classList.remove("hidden");
 };
 
-function clearAffirm() {
-    affirmButton.checked = false;
+function outputRandom() {
+    if (mantraButton.checked) {
+        displayMessage();
+        displayedMessage.innerText = mantras[getRandomIndex(mantras)];
+        mantraButton.checked = false;
+    } else if (affirmButton.checked) {
+        displayMessage();
+        displayedMessage.innerText = affirmations[getRandomIndex(affirmations)];
+        affirmButton.checked = false;
+    } else {
+        return;
+    };
 };
