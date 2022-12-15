8cc01e7c6b1a98625ce21a52d581e705de667a4d
Amber Shipley <ashipley2@me.com>
2022-12-14 16:19:16 -0800

    feat: Create randomizers for affirmations and mantras

---
+++ main.js
@@ -0,0 +1,3 @@
+var randomAffirmation = affirmations[Math.floor(Math.random() * affirmations.length)];
+var randomMantras = mantra[Math.floor(Math.random() * mantras.length)];
+console.log(randomAffirmation)
