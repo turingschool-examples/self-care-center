74d29e91d58ac0920e802284b2de7414b55fdd37
Amber Shipley <ashipley2@me.com>
2022-12-15 13:54:56 -0800

    Add event listeners for radio button selections

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 var randomAffirmation = affirmations[Math.floor(Math.random() * affirmations.length)];
-var randomMantras = mantra[Math.floor(Math.random() * mantras.length)];
-console.log(randomAffirmation)
+var randomMantra = mantra[Math.floor(Math.random() * mantras.length)];
 
-var messageBox = document.querySelector('.message-pop-up')
-var receiveBtn = document.querySelector('.receive-btn')
+var messageBox = document.querySelector('.message-pop-up');
+var receiveBtn = document.querySelector('.receive-btn');
+var meditate = document.querySelector('.meditate');
+var affirmRadio = document.querySelector('#affirmation');
+var mantraRadio = document.querySelector('#mantra');
 
-
-// receiveBtn.addEventListener('click', showMessage)
+affirmRadio.addEventListener('click', changeSelection);
+mantraRadio.addEventListener('click', changeSelection);
+// receiveBtn.addEventListener('click', hideMeditate);
+function changeSelection() {
+    if (document.getElementById('affirmation')) {
+        messageBox.innerHTML = randomAffirmation;
+    } else {
+        messageBox.innerHTML = randomMantra;
+    }
+}
