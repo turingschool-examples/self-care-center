63bdbf0f77de05135f94a350579b493371025d1d
Katherine Blaine <blaine.katherine12@gmail.com>
2022-12-15 17:12:36 -0600

    Add hide/show functions

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 // Variables -
 
-var receiveMessageButton = document.querySelector('button')
-
+var receiveMessageButton = document.querySelector('#receive-message-button')
+var textBoxOne = document.querySelector('.textBox1')
 var affirmationRadio = document.querySelector('#affirmation-selector')
 var mantraRadio = document.querySelector('#mantra-selector')
 var textBoxTwo = document.querySelector('.textBox2')
 var meditationIcon = document.querySelector('img')
 var messageBox = document.querySelector('.message')
-var favoriteButton = document.querySelector('.favoriteButton')
+var savedMessageSection = document.querySelector('.saved-messages')
+var saveMessageButton = document.querySelector('#view-saved-messages')
+var h2 = document.querySelector('.h2')
+var mainPage = document.querySelector('#main-page')
 
 var message;
 
@@ -20,40 +23,53 @@ receiveMessageButton.addEventListener('click', function() {
     displayMessage()
 })
 
-favoriteButton.addEventListener('click', saveMessage())
-
+saveMessageButton.addEventListener('click', showSavedMessages)
 
 // Event Handlers -
 
 function displayMessage() {
     if (affirmationRadio.checked === true) {
         message = affirmations[getRandomIndex(affirmations)];
-        console.log(message)
-        textBoxTwo.innerHTML = `
-        <section>
-            <p>${message}</p>
-            <button class='favorite-button'>Save Message</button>
-        </section>`
     } else if (mantraRadio.checked === true) {
         message = mantras[getRandomIndex(mantras)];
-        textBoxTwo.innerHTML = `
-        <section>
-            <p>${message}</p>
-            <button class='favorite-button'>Save Message</button>
-        </section>`
     } else {
         message = 'Please select a message option!'
         textBoxTwo.innerHTML = `
         <p>${message}</p>`
-    }
+        return
+    } textBoxTwo.innerHTML = `
+    <section>
+        <p>${message}</p>
+        <button class='button-format'>Save Message</button>
+    </section>`
+
+    var favoriteButton = document.querySelector('.favorite-button')
+    console.log(favoriteButton)
+    favoriteButton.addEventListener('click', function() {
+        saveMessage(message)})
 }
 
-function saveButton(message) {
+function saveMessage(message) {
     savedMessages.push(message)
+}
+
+var image = document.querySelector('.img')
+
+function showSavedMessages() {
+    show(savedMessageSection)
+    hide(mainPage)
 }
 
 // Misc. Functions -
 
 function getRandomIndex(array) {
     return Math.floor(Math.random() * array.length)
+}
+
+function show(element) {
+    element.classList.remove('hidden')
+}
+
+function hide(element) {
+    element.classList.add('hidden')
 }
