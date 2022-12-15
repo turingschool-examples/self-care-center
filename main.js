5e02bdf2194993ab2f12c1f1493049e9cab8acc3
Amber Shipley <ashipley2@me.com>
2022-12-14 18:01:17 -0800

    Add variables and begin event listeners

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 var randomAffirmation = affirmations[Math.floor(Math.random() * affirmations.length)];
 var randomMantras = mantra[Math.floor(Math.random() * mantras.length)];
 console.log(randomAffirmation)
+
+var messageBox = document.querySelector('.message-pop-up')
+var receiveBtn = document.querySelector('.receive-btn')
+
+
+// receiveBtn.addEventListener('click', showMessage)
