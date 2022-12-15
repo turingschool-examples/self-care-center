bda7e4ecce260074d3fc92a683d8e88ee6f21104
Jason Kirchman <kirchman.jason@icloud.com>
2022-12-14 20:25:54 -0600

    feat: Create variables linked to DOM elements and add event listener to receive message button

---
+++ main.js
@@ -0,0 +1,8 @@
+var receiveMessageBtn = document.querySelector('#btn-receive-msg');
+var affirmationRb = document.querySelector('#rb-affirmation');
+var mantraRb = document.querySelector('#rb-mantra');
+var messageDisplay = document.querySelector('#message-display');
+
+receiveMessageBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+});
