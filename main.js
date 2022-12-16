a1c5ad1d3b1a18912936ffce04cc8e511bf31e26
Chrissy Cooper <cooper.christine.r@gmail.com>
2022-12-16 09:21:55 -0600

    Clean up synthax for basic random sentiment functionality

--- main.js
+++ main.js
@@ -1,31 +1,19 @@
-
-//there are two radio buttons that should switch which array we are randomizing on click of the recieve message button
-    //how do forms work?
-        //is there any built in functionality in the forms html?
-        //do I grab each radio button with a querySelector?
-        //Do i use an event.target
-        //loop through the node array and see which one is checked?
-
 var logo = document.querySelector(".logo");
 var messageDiv = document.querySelector(".messagediv");
 var messageButton = document.querySelector(".button");
 var radioButtons = document.querySelectorAll(".radio");
 var form = document.querySelector("form");
 
-
-
 messageButton.addEventListener("click", preventDefault);
-messageButton.addEventListener("click", function(){
-    hide(logo);
-    displaySentiment();
-});
-
+messageButton.addEventListener("click", displaySentiment);
 
 function displaySentiment() {
     if(radioButtons[0].checked) {
-        randomAff = affirmations[getRandomIndex(affirmations)];
-        messageDiv.innerText = randomAff; 
+      hide(logo);
+      randomAff = affirmations[getRandomIndex(affirmations)];
+      messageDiv.innerText = randomAff; 
     } else if (radioButtons[1].checked) {
+      hide(logo);
       randomMantra = mantras[getRandomIndex(mantras)];
       messageDiv.innerText = randomMantra; 
     }
