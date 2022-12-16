073be30e261e565a7c232253c9ac5e3e3e43b241
Trey Marshall <marshall.trey92@yahoo.com>
2022-12-16 15:20:09 -0600

    Add data.js and begin Javascript

---
+++ main.js
@@ -0,0 +1,12 @@
+var affirmationButton = document.querySelector('.affir-button');
+var mantraButton = document.querySelector('.mantra-button');
+
+var messageArray = [];
+
+
+
+function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+  };
+
+console.log(array)  
