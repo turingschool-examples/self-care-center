b8f24c817106e8024de2cba4c1e1146534a6e3c7
Sara Park <sspark106@gmail.com>
2022-12-15 17:30:48 -0700

    feature: Display form with clicking add message button

--- main.js
+++ main.js
@@ -16,12 +16,14 @@ var mantras = [
 
 var receiveMessageBtn = document.querySelector('.receive-message-btn');
 var affirmationBtn = document.querySelector('.affirmation-button');
+var affirmationBtn2 = document.querySelector('.affirmation-button-2');
 var mantraBtn = document.querySelector('.mantra-button');
+var mantraBtn2 = document.querySelector('.mantra-button-2');
 var message = document.querySelector('.message-box');
 var icon = document.querySelector('svg');
 var addMessageBtn = document.querySelector('.add-message');
-var form = document.querySelector('form');
-
+var submitBtn = document.querySelector('.submit');
+var textArea = document.querySelector('.message-input');
 
 receiveMessageBtn.addEventListener('click', displayMessage);
 
@@ -29,10 +31,10 @@ function displayMessage() {
     var randomAffirmIndex = Math.floor(Math.random() * affirmations.length);
     var randomMantraIndex = Math.floor(Math.random() * mantras.length);
     icon.classList.add('hidden');
-    if (affirmationBtn.checked && !mantraBtn.checked) {
+    if (affirmationBtn.checked) {
         message.innerHTML = `<p>${affirmations[randomAffirmIndex]}</p>
         <button class="add-message">Add Message</button>`;
-    } else if (mantraBtn.checked && !affirmationBtn.checked) {
+    } else if (mantraBtn.checked) {
         message.innerHTML = `<p>${mantras[randomMantraIndex]}</p>
         <button class="add-message">Add Message</button>`;
     } else {
@@ -43,11 +45,38 @@ function displayMessage() {
 addMessageBtn.addEventListener('click', displayForm);
 
 function displayForm() {
-    if (affirmations[randomAffirmIndex] || mantras[randomMantraIndex]) {
-        message.innerHTML = ` `;
-        form.classList.remove('hidden');
-    }  
-}
+    message.innerHTML = ' ';
+    message.innerHTML = `
+        <form>
+            <input type="radio" name="option" class="affirmation-button-2"/>
+            <label for="affirmation"><em>affirmation</em></label><br>
+            <input type="radio" name="option" class="mantra-button-2"/>
+            <label for="mantra" class=mantra-option-2><em>mantra</em></label><br>
+            <textarea type="text" class="message-input"></textarea><br>
+            <button class="submit">Submit</button>
+        </form>
+        `;
+    icon.classList.add('hidden');
+};
+
+submitBtn.addEventListener('click', addMessageToList);
+
+function addMessageToList(event) {
+    event.preventDefault();
+    displayForm();
+    if (affirmationBtn2.checked && textArea.value) {
+        affirmations.push(`${textArea.value}`);
+        message.innerHTML = `${textArea.value}`;
+        icon.classList.add('hidden');
+    } else if (mantraBtn2.checked && textArea.value) {
+        mantras.push(textArea.value);
+        message.innerHTML = `${textArea.value}`
+    } else {
+        alert("Please select either affirmation or mantra, and input message");
+    }
+};
+
+
 
 
 
