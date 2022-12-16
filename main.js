074b82fd7e9a3dd91b649ba3d7b041db1d45256f
Trey Marshall <marshall.trey92@yahoo.com>
2022-12-16 17:11:26 -0600

    Complete and refactor iteration 1

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
-var affirmationButton = document.querySelector('.affir-button');
-var mantraButton = document.querySelector('.mantra-button');
+var selectionButton = document.querySelector('.message-button');
+var receiveButton = document.querySelector('.receive-button')
+var messageArea = document.querySelector('#message-box')
 
-var messageArray = [];
+var currentMessage;
 
-
+receiveButton.addEventListener('click', receiveMessage)
 
 function getRandomIndex(array) {
     return Math.floor(Math.random() * array.length);
   };
 
-console.log(array)  
+
+function receiveMessage(event) {
+    event.preventDefault()
+    if (selectionButton.checked === true) {
+    var randomAffirmation = getRandomIndex(affirmations)
+    currentMessage = affirmations[randomAffirmation]
+    } else if (selectionButton.checked === false) {
+    var randomMantra = getRandomIndex(mantras)
+    currentMessage = mantras[randomMantra]   
+    }
+    messageArea.innerHTML = `<p>${currentMessage}</p>` 
+}
+
+function deleteMessage() {
+
+}
+
+// User can delete a message
+// Add the ability to delete a messge (ie: when a message shows up, show a button that says “I don’t like this messge” (or something similar), and remove it from the list so that it will not show up any more. Make sure to alert the user in some way that the message has been removed.
+
+
+
+
