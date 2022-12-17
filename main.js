6af64073460b23a6880967c642c654dd510bd734
Travis Parks <travisparks0788@gmail.com>
2022-12-17 13:56:44 -0500

    feat completed cyoa

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-var Affirmations = ["I forgive myself and set myself free.", "I believe I can be all that I want to be.", "I am in the process of becoming the best version of myself.", 
+var affirmations = ["I forgive myself and set myself free.", "I believe I can be all that I want to be.", "I am in the process of becoming the best version of myself.", 
 "I have the freedom & power to create the life I desire.", "I choose to be kind to myself and love myself unconditionally.", "My possibilities are endless.",
 "I am worthy of my dreams.", "I am enough.", "I deserve to be healthy and feel good.", "I am full of energy and vitality and my mind is calm and peaceful.", 
 "Every day I am getting healthier and stronger.", "I honor my body by trusting the signals that it sends me.",
 "I manifest perfect health by making smart choices."]
 
 
-var Mantras = ["Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.", "Don/t let yesterday take up too much of today.", "Every day is a second chance.", 
+var mantras = ["Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.", "Don\'t let yesterday take up too much of today.", "Every day is a second chance.", 
 "Tell the truth and love everyone.", "I am free from sadness.", "I am free from sadness.", " I am enough.", "I am enough.", "In the beginning it is you, in the middle it is you and in the end it is you.", 
 "I love myself.", "I am present now.", "Inhale the future, exhale the past.", "This too shall pass.", "Yesterday is not today.", "The only constant is change.", 
 "Onward and upward.", " I am the sky, the rest is weather."]
@@ -37,10 +37,10 @@ function getRandomIndex(array) {
 
 function generateMessage() {
   if (affirmationButton.checked) {
-    displaySaying.innerText = Affirmations[getRandomIndex(Affirmations)]                  
+    displaySaying.innerText = affirmations[getRandomIndex(affirmations)]                  
     yogi.classList.add("hidden")
   } else if (mantraButton.checked) {
-    displaySaying.innerText = Mantras[getRandomIndex(Mantras)]
+    displaySaying.innerText = mantras[getRandomIndex(mantras)]
     yogi.classList.add("hidden")
     } else {
         displaySaying.innerText = ("Please choose Affirmation or Mantra message. Namaste. 🧘 ");
@@ -52,18 +52,24 @@ function createOwnMessage() {
   yogi.classList.add("hidden")
   userForm.classList.remove("hidden")
   hideTopinfo.classList.add("hidden")
+  displaySaying.classList.add("hidden")
 }
+
 
 function displayUserMessage() {
 event.preventDefault()
   if (userAffirmationButton.checked) {
     displaySaying.innerText = userAffirmationSaying.value
-    Affirmations.push(userAffirmationSaying.value)
+    affirmations.push(userAffirmationSaying.value)
     userForm.classList.add("hidden")
+    displaySaying.classList.remove("hidden")
+    hideTopinfo.classList.remove("hidden")
   } else if (UserMantraButton.checked) {
     displaySaying.innerText = userMantraSaying.value
-    Mantras.push(userMantraSaying.value)
+    mantras.push(userMantraSaying.value)
     userForm.classList.add("hidden")
+    displaySaying.classList.remove("hidden")
+    hideTopinfo.classList.remove("hidden")
   } else  {
     alert("Please choose Affirmation or Mantra message. Namaste. 🧘 ");
     }
