485a415b4bd5f818cb76516b58da9a7b72368f67
“Saki <sakisandra.c@gmail.com>
2023-04-01 18:00:06 -0600

    Add mantras to data.js

--- data.js
+++ data.js
@@ -12,4 +12,21 @@ let affirmations = [
     'Every day I am getting healthier and stronger.', 
     'I honor my body by trusting the signals that it sends me.', 
     'I manifest perfect health by making smart choices.'
+];
+
+let mantras = [
+    'Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.', 
+    'Don’t let yesterday take up too much of today.','Every day is a second chance.',
+    'Tell the truth and love everyone.',
+    'I am free from sadness.',
+    'I am enough.',
+    'In the beginning it is you, in the middle it is you and in the end it is you.',
+    'I love myself.',
+    'I am present now.',
+    'Inhale the future, exhale the past.',
+    'This too shall pass.',
+    'Yesterday is not today.',
+    'The only constant is change.',
+    'Onward and upward.',
+    'I am the sky, the rest is weather.'
 ]
