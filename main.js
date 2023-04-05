aeb0d0a2b36ccd73483cfa024c19f6e2d098c785
Laura Guerra <l.garciaguerra1@gmail.com>
2023-04-05 09:49:24 -0400

    Refactor display to be dynamic

--- main.js
+++ main.js
@@ -35,14 +35,31 @@ var mantras = [
 var msgBtn = document.querySelector('.show-message-button')
 var message = document.querySelector('.message-view')
 var icon = document.querySelector('.icon-view')
+var affirmation = document.getElementById('affirmation-radio')
+var mantra = document.getElementById('mantra-radio')
+
 msgBtn.addEventListener('click', displayMsg)
 
+function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+  }
+
 function displayMsg() {
-message.classList.remove('hidden')
-icon.classList.add('hidden')
-if (document.getElementById('affirmation-radio').checked) {
-    message.innerText = affirmations[0];
-} else if (document.getElementById('mantra-radio').checked) {
-    message.innerText = mantras[0]
+    var messageChosen = chooseMsg();
+    if (messageChosen) {
+        message.classList.remove('hidden');
+        icon.classList.add('hidden');
+    }
+}
+
+function chooseMsg() {
+if (affirmation.checked) {
+    var idx = getRandomIndex(affirmations);
+    message.innerText = affirmations[idx];
+    return true;
+} else if (mantra.checked) {
+    var idx = getRandomIndex(mantras);
+    message.innerText = mantras[idx]
+    return true;
 }
 }
