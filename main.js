d7f4f03cc897dcd98042b32ddb13e28fad5e4188
Jason Alberto <alberto.jason@gmail.com>
2023-04-06 14:17:52 -0400

    Save the message with button and show view saved message button

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 var affirmationRadio = document.querySelector('.affirmation-message');
 var mantraRadio = document.querySelector('.mantra-message');
 var createMessageButton = document.querySelector('.receive-message');
-var saveMessageButton = document.querySelector('.save-message')
+var saveMessageButton = document.querySelector('.save-message');
+var viewSavedMessages = document.querySelector('.view-saved')
 var createMessageBox = document.querySelector('.display-message');
 
+var savedMessages = [];
+var currentMessage;
+
 createMessageButton.addEventListener('click', selectMessage);
-console.log(createMessageBox.innerHTML === '<img src="./assets/meditate.svg" alt="enlightened buddha">')
+saveMessageButton.addEventListener('click', function() {
+    saveMessage();
+
+    if (savedMessages.length) {
+        viewSavedMessages.classList.remove('hidden');
+    }
+});
+
+
 function selectMessage() {
     if (affirmationRadio.checked) {
         
@@ -17,16 +29,17 @@ function selectMessage() {
 
     } else if (mantraRadio.checked) {
         
-        if (createMessageBox.innerHTML === '<img src="./assets/meditate.svg" alt="enlightened buddha">') {
+        if (createMessageBox.innerHTML === `<img src="./assets/meditate.svg" alt="enlightened buddha">`) {
             toggleHiddenButton(saveMessageButton);
         }
-        
+
         createMessageBox.innerHTML = `<p>${createMessage(mantras)}</p>`;
     }
 }
 
 function createMessage(messageType) {
-    return messageType[getRandomMessage(messageType)];
+    currentMessage = messageType[getRandomMessage(messageType)];
+    return currentMessage;
 }
 
 function getRandomMessage(messageType) {
@@ -35,4 +48,10 @@ function getRandomMessage(messageType) {
 
 function toggleHiddenButton(button) {
     button.classList.toggle('hidden');
+}
+
+function saveMessage() {
+    if (!savedMessages.includes(currentMessage)) {
+        savedMessages.push(currentMessage);
+    }
 }
