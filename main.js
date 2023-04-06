733c7993e6ab0776263c7350ca58560fbb3d7768
Jason Alberto <alberto.jason@gmail.com>
2023-04-05 22:55:42 -0400

    Add function to randomize message depending on user input

--- main.js
+++ main.js
@@ -7,14 +7,15 @@ createMessageButton.addEventListener('click', selectMessage);
 
 function selectMessage() {
     if (affirmationRadio.checked) {
-       
+       createMessage(affirmations);
     } else if (mantraRadio.checked) {
-       
+       createMessage(mantras);
     }
 }
 
-function createMessage() {
-    
+function createMessage(messageType) {
+    var messagePosition = getRandomMessage(messageType);
+    return messageType[messagePosition];
 }
 
 function getRandomMessage(messageType) {
