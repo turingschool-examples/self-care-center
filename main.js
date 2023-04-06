1c62baa15b89bebfc0c95ba5afa99073ea2c3cab
Caitlin Cradick <caitlincradick@gmail.com>
2023-04-05 20:08:42 -0700

    feat:adds form page, sytles it and hides it in JS

--- main.js
+++ main.js
@@ -2,15 +2,21 @@
 var affirmationButton = document.getElementById('affirmation');
 var mantraButton = document.getElementById('mantra');
 var recieveMessageButton = document.querySelector('.recieveMessageBtn');
-var icon = document.querySelector('.image');
+var icon = document.querySelector('.mainImage');
 var message = document.querySelector('.message');
-var createMessageButton = document.querySelector('.createMessageBtn')
+var createMessageButton = document.querySelector('.createMessageBtn');
+var mainPage = document.querySelector('.mainPage');
+var formPage = document.querySelector('.formPage');
 
 //EVENT LISTENERS
 recieveMessageButton.addEventListener('click', function() {
 showMessage();
 getMessage();
 printMessage();
+});
+
+createMessageButton.addEventListener('click', function() {
+hideMainPage();
 });
 
 var affirmationArray = ['I forgive myself and set myself free.','I believe I can be all that I want to be.','I am in the process of becoming the best version of myself.','I have the freedom & power to create the life I desire.','I choose to be kind to myself and love myself unconditionally.','My possibilities are endless.','I am worthy of my dreams.','I am enough.','I deserve to be healthy and feel good.','I am full of energy and vitality and my mind is calm and peaceful.','Every day I am getting healthier and stronger.','I honor my body by trusting the signals that it sends me.','I manifest perfect health by making smart choices.'
@@ -49,3 +55,7 @@ function printMessage(){
     }
 }
 
+function hideMainPage(){
+    mainPage.classList.add('hidden');
+    formPage.classList.remove('hidden');
+}
