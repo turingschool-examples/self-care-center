1e4df6b6e9dfd55b4d5587ccf0f5ae5e6e736e82
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-04-05 21:10:22 -0600

    Begin pseudo-coding for functionality and start building functions

--- main.js
+++ main.js
@@ -3,6 +3,16 @@ var mantras = ["Breathing in, I send myself love. Breathing out, I send love to 
 
 var affirmations = ["I forgive myself and set myself free.", "I believe I can be all that I want to be.", "I am in the process of becoming the best version of myself.", "I have the freedom & power to create the life I desire.", "I choose to be kind to myself and love myself unconditionally.", "My possibilities are endless.", "I am worthy of my dreams.", "I am enough.", "I deserve to be healthy and feel good.", "I am full of energy and vitality and my mind is calm and peaceful.", "Every day I am getting healthier and stronger.", "I honor my body by trusting the signals that it sends me.", "I manifest perfect health by making smart choices."];
 
+//Query Selectors
+var form = document.addQuerySelector('form')
+var selection = document.addQuerySelector('#selection')
+
 //Event Listeners
+form.addEventListener('submit', generateMessage)
 
 //Functions
+function generateMessage(){
+    
+}
+
+//event listener for the "submit" (aka button click on recieve messae button) then invokes function that uses the variable that stores the value of the selection to use an if else statement to either display a mantra or a affirmation
