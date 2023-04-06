3c5a675971b54436df258e8cab6b3c89d54f7ec5
Mike Gudenau <mikegudenau@gmail.com>
2023-04-06 09:55:21 -0700

    Display affirmation message upon button click

--- main.js
+++ main.js
@@ -14,66 +14,81 @@
 var receiveButton = document.querySelector(".receive");
 var radioAffirmation = document.querySelector("#affirmation");
 var meditationImg = document.querySelector("img");
-var messageDisplaySection = document.querySelector('.message-display')
+var messageDisplaySection = document.querySelector(".message-display");
 
 // create an event listener
-receiveButton.addEventListener("click", displayMessage);
-radioAffirmation.addEventListener("click", displayMessage);
+receiveButton.addEventListener("click", renderAffirmationMessage);
 
 var currentMessage;
 
 // random array index
 function getRandomIndex(array) {
-    return Math.floor(Math.random() * array.length);
+  return Math.floor(Math.random() * array.length);
 }
 
-function createMessage()
+function createMessage(quote) {
+  var message = {
+    id: Date.now(),
+    quote: quote,
+  };
+  return message;
+}
 
+// function randomAffirmationMessage() {
+//   currentMessage = createMessage(affirmations[getRandomIndex(affirmations)]);
+//   return currentMessage;
+// }
+
+function renderAffirmationMessage() {
+  hide(meditationImg);
+  for (var i = 0; i < affirmations.length; i++) {
+    messageDisplaySection.innerHTML = `
+    <section class="display-container">
+    <h3>${affirmations[getRandomIndex(affirmations)]}</h3>
+    </section>`
+  }
+//   console.log(randomAffirmationMessage());
+}
 
 function hide(element) {
-    element.classList.add("hidden");
-  }
-  
-  function show(element) {
-    element.classList.remove("hidden");
-  }
-  
-  function displayMessage() {
-    console.log("hello");
-    hide(meditationImg);
-  }
+  element.classList.add("hidden");
+}
 
-// array objects data model
+function show(element) {
+  element.classList.remove("hidden");
+}
+
+// array data model
 var affirmations = [
-    'I forgive myself and set myself free.',
-    'I believe I can be all that I want to be.',
-    'I am in the process of becoming the best version of myself.',
-    'I have the freedom & power to create the life I desire.',
-    'I choose to be kind to myself and love myself unconditionally.',
-    'My possibilities are endless.',
-    'I am worthy of my dreams.',
-    'I am enough.',
-    'I deserve to be healthy and feel good.',
-    'I am full of energy and vitality and my mind is calm and peaceful.',
-    'Every day I am getting healthier and stronger.',
-    'I honor my body by trusting the signals that it sends me.',
-    'I manifest perfect health by making smart choices.'
-]
+  "I forgive myself and set myself free.",
+  "I believe I can be all that I want to be.",
+  "I am in the process of becoming the best version of myself.",
+  "I have the freedom & power to create the life I desire.",
+  "I choose to be kind to myself and love myself unconditionally.",
+  "My possibilities are endless.",
+  "I am worthy of my dreams.",
+  "I am enough.",
+  "I deserve to be healthy and feel good.",
+  "I am full of energy and vitality and my mind is calm and peaceful.",
+  "Every day I am getting healthier and stronger.",
+  "I honor my body by trusting the signals that it sends me.",
+  "I manifest perfect health by making smart choices.",
+];
 
 var mantras = [
-    'Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.',
-    "Don’t let yesterday take up too much of today.",
-    'Every day is a second chance.',
-    'Tell the truth and love everyone',
-    'I am free from sadness.',
-    'I am enough',
-    'In the beginning it is you, in the middle it is you and in the end it is you.',
-    'I love myself.',
-    'I am present now.',
-    'Inhale the future, exhale the past.',
-    'This too shall pass.',
-    'Yesterday is not today.',
-    'The only constant is change.',
-    'Onward and upward.',
-    'I am the sky, the rest is weather.'
-]
+  "Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.",
+  "Don’t let yesterday take up too much of today.",
+  "Every day is a second chance.",
+  "Tell the truth and love everyone",
+  "I am free from sadness.",
+  "I am enough",
+  "In the beginning it is you, in the middle it is you and in the end it is you.",
+  "I love myself.",
+  "I am present now.",
+  "Inhale the future, exhale the past.",
+  "This too shall pass.",
+  "Yesterday is not today.",
+  "The only constant is change.",
+  "Onward and upward.",
+  "I am the sky, the rest is weather.",
+];
