263a2c292a37eb878e28d48d2d124976459efdb6
Laura Guerra <l.garciaguerra1@gmail.com>
2023-04-05 20:53:05 -0400

    Update function to display only unique messages

--- main.js
+++ main.js
@@ -10,21 +10,51 @@ function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
 
+var displayedMantras = [];
+var displayedAffirmations = [];
+
+function organizeAffirmations(i) {
+  displayedAffirmations.push(affirmations[i]);
+  affirmations.splice(i, 1);
+}
+
+function organizeMantras(i) {
+  displayedMantras.push(mantras[i]);
+  mantras.splice(i, 1);
+}
+
 function displayMsg() {
   if (chooseMsg()) {
     message.classList.remove('hidden');
     icon.classList.add('hidden');
+  } else {
+    icon.classList.remove('hidden');
+    message.classList.add('hidden');
   }
 }
 
 function chooseMsg() {
   if (affirmation.checked) {
-    var idx = getRandomIndex(affirmations);
-    message.innerText = affirmations[idx];
-    return true;
+    if (affirmations.length) {
+      var idx = getRandomIndex(affirmations);
+      message.innerText = affirmations[idx];
+      organizeAffirmations(idx);
+      return true;
+    } else {
+      alert(`You have seen all the mantras we currently have to offer. You will now start begin seeing repeated mantras.`);
+      affirmations = displayedAffirmations;
+      displayedAffirmations = [];
+    }
   } else if (mantra.checked) {
-    var idx = getRandomIndex(mantras);
-    message.innerText = mantras[idx];
-    return true;
+    if (mantras.length) {
+      var idx = getRandomIndex(mantras);
+      message.innerText = mantras[idx];
+      organizeMantras(idx);
+      return true;
+    } else {
+      alert(`You have seen all the affirmations we currently have to offer. You will now start begin seeing repeated affirmations.`);
+      mantras = displayedMantras;
+      displayedMantras = [];
+    }
   }
 }
