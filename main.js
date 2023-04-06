e4d3ba48f0e7f0223efd9065cefac84a7e4ba176
Hollis Vohr <hollis.vohr@gmail.com>
2023-04-05 18:13:15 -0600

    Add event listener, corresponding functions, and querySelectors for affirmations

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ var affirmations = [
     "Every day I am getting healthier and stronger.",
     "I honor my body by trusting the signals that it sends me.",
     "I manifest perfect health by making smart choices."
-]
+];
 
 var mantras = [
     "Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.",
@@ -30,4 +30,58 @@ var mantras = [
     "The only constant is change.",
     "Onward and upward.",
     "I am the sky, the rest is weather."
-]
+];
+
+// DOM queries:
+
+var recieveMessageButton = document.querySelector(".recieve-message-button");
+var affirmationRadio = document.querySelector("#affirmation-radio");
+var mantraRadio = document.querySelector("#mantra-radio");
+var meditatingImage = document.querySelector(".meditating-person");
+var userChosenMessage = document.querySelector(".user-random-phrase");
+
+// event listeners:
+
+recieveMessageButton.addEventListener("click", recieveMessage)
+
+// functions:
+
+
+function showAffirmation() {
+    var myText;
+    for (i = 0; i < userChosenMessage.length; i++) {
+        if (userChosenMessage[i].innerHTML == "test") {
+            // console.log(myText, p[0].innerHTML);
+            myText = userChosenMessage[i];
+            break;
+        }
+    }
+    userChosenMessage.removeAttribute("hidden");
+    meditatingImage.setAttribute("hidden", "");
+};
+
+
+function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+};
+
+
+function randomAffirmation() {
+    var newAffirmations = affirmations[getRandomIndex(affirmations)];
+   return newAffirmations;
+};
+
+console.log(randomAffirmation());
+
+function recieveMessage() {
+    userChosenMessage.innerHTML = '';
+    console.log(affirmationRadio);
+        if (affirmationRadio.checked) {
+            userChosenMessage.innerHTML +=
+                `<p class= "user-random-phrase">${randomAffirmation()}</p>`
+                showAffirmation();
+        }
+
+    }
+
+
