71f1d39f5bc72d86df1939901b42300cfedcd12f
Jason Alberto <alberto.jason@gmail.com>
2023-04-06 10:36:08 -0400

    Add save message button visibility

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 var affirmationRadio = document.querySelector('.affirmation-message');
 var mantraRadio = document.querySelector('.mantra-message');
-var createMessageButton = document.querySelector('button');
-var createMessageBox = document.querySelector('.message');
+var createMessageButton = document.querySelector('.receive-message');
+var saveMessageButton = document.querySelector('.save-message')
+var createMessageBox = document.querySelector('.display-message');
 
 createMessageButton.addEventListener('click', selectMessage);
 
 function selectMessage() {
     if (affirmationRadio.checked) {
-       createMessageBox.innerHTML = `<p>${createMessage(affirmations)}</p>`
+       createMessageBox.innerHTML = `<p>${createMessage(affirmations)}</p>`;
+
+       if (createMessageBox.innerHTML === '<img src="./assets/meditate.svg" alt="enlightened buddha">') {
+           toggleHiddenButton(saveMessageButton);
+       }
+
     } else if (mantraRadio.checked) {
-       createMessageBox.innerHTML = `<p>${createMessage(mantras)}</p>`
+       createMessageBox.innerHTML = `<p>${createMessage(mantras)}</p>`;
+       
+       if (createMessageBox.innerHTML === '<img src="./assets/meditate.svg" alt="enlightened buddha">') {
+           toggleHiddenButton(saveMessageButton);
+       }
     }
 }
 
@@ -19,4 +29,8 @@ function createMessage(messageType) {
 
 function getRandomMessage(messageType) {
     return Math.floor(Math.random() * messageType.length);
+}
+
+function toggleHiddenButton(button) {
+    button.classList.toggle('hidden');
 }
