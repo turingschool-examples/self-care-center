3c870f6c1073195a62c7a44b4f3e8a3d24947da6
Jamie Caudill <j.caudill7177@gmail.com>
2023-04-06 16:54:39 -0700

    Write pseudocode for receive message feature

---
+++ main.js
@@ -0,0 +1,8 @@
+// Goal: When the user clicks on the receive message button, the corresponding mantra or affirmation will display in place of the meditation icon.
+
+// Steps:
+    // Make a function that returns a random index.
+    // Make a function that checks to see if the mantra or affirmation selector is picked, and then uses the random index function to pick a mantra or affirmation.
+    // Make an event listener that responds to click.
+    // update HTML to hide the buddha icon and display text
+    // update CSS to style text. 
