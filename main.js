b0a0f2c1e24ec22005aea78a2d0fd9feab5eb6c4
Jason Alberto <alberto.jason@gmail.com>
2023-04-07 12:52:27 -0400

    Refactoring code to remove innerHTML to keep event listeners working work ongoing

--- main.js
+++ main.js
@@ -1,23 +1,35 @@
 var affirmationRadio = document.querySelector('.affirmation-message');
 var mantraRadio = document.querySelector('.mantra-message');
-var createMessageButton = document.querySelector('.receive-message');
+var createMessageButton = document.querySelector('.create-message');
 var favouriteMessageButton = document.querySelector('.save-message');
 var viewFavouriteButton = document.querySelector('.view-favourite');
 var viewHomeButton = document.querySelector('.home');
+var deleteFavouriteButton = document.querySelector('.delete-message');
 var mantraSelectBox = document.querySelector('.mantra-select');
 var messageContainer = document.querySelector('.receive-messages');
 var subTitle = document.querySelector('.sub-title');
+var favouriteSection = document.querySelector('.favourite-section');
+var messageHtmlParagraph = document.querySelector('.message');
+var imgBuddha = document.querySelector('.img-buddha');
+
+var createMessageBox = document.querySelector('.display-message');
 
 var savedMessages = [];
 var currentMessage;
 
+// VARIABLES TO GENERATE HTML
+
+var newHtmlParagraph = document.createElement('p');
+var newHtmlButton = document.createElement('button');
+var newHtmlSection = document.createElement('section');
+var newHtmlImg = document.createElement('img');
+var newHtmlDiv = document.createElement('div');
+
 // EVENT LISTENERS
 
-createMessageButton.addEventListener('click', function(event){
-        selectMessage();
-    event.preventDefault();
-});
+createMessageButton.addEventListener('click', selectMessage);
 favouriteMessageButton.addEventListener('click', function() {
+    favouriteMessageButton = document.querySelector('.save-message');
     favouriteMessage();
     if (savedMessages.length) {
         viewFavouriteButton.classList.remove('hidden');
@@ -27,6 +39,8 @@ viewFavouriteButton.addEventListener('click', function() {
     toggleHiddenButton(mantraSelectBox);
     toggleHiddenButton(viewHomeButton);
     toggleHiddenButton(viewFavouriteButton);
+    toggleHiddenButton(messageContainer);
+    toggleHiddenButton(favouriteSection);
     viewFavouriteMessages();
     swapSubTitle();
 });
@@ -34,28 +48,54 @@ viewHomeButton.addEventListener('click', function() {
     toggleHiddenButton(mantraSelectBox);
     toggleHiddenButton(viewHomeButton);
     toggleHiddenButton(viewFavouriteButton);
+    toggleHiddenButton(messageContainer);
+    toggleHiddenButton(favouriteSection);
+    toggleHiddenButton(imgBuddha);
+    toggleHiddenButton(messageHtmlParagraph);
+    toggleHiddenButton(favouriteMessageButton);
     viewHome();
     swapSubTitle();
+});
+deleteFavouriteButton.addEventListener('click', function() { 
+    // deleteFavouriteButton = document.querySelector('.delete-message');
+    deleteFavourite();
 });
 
 // FUNCTIONS
 
 function selectMessage() {
-    var createMessageBox = document.querySelector('.display-message');
-
+    createMessageBox = document.querySelector('.display-message');
+    // favouriteMessageButton = document.querySelector('.save-message');
+    // favouriteMessageButton = document.querySelector('.save-message');
+    // console.log(createMessageBox.innerHTML === `<img class="img-buddha" src="./assets/meditate.svg" alt="enlightened buddha">`)
+    console.log(!imgBuddha.classList.contains('hidden'))
     if (affirmationRadio.checked) {
-        if (createMessageBox.innerHTML === `<img src="./assets/meditate.svg" alt="enlightened buddha">`) {
+        if (!imgBuddha.classList.contains('hidden')) {
+            toggleHiddenButton(imgBuddha);
+            toggleHiddenButton(messageHtmlParagraph);
             toggleHiddenButton(favouriteMessageButton);
         }
-
-        createMessageBox.innerHTML = `<p>${createMessage(affirmations)}</p>`;
+        
+        messageHtmlParagraph.innerText = createMessage(affirmations);
 
     } else if (mantraRadio.checked) {
-        if (createMessageBox.innerHTML === `<img src="./assets/meditate.svg" alt="enlightened buddha">`) {
+        if (!imgBuddha.classList.contains('hidden')) {
+            toggleHiddenButton(imgBuddha);
+            toggleHiddenButton(messageHtmlParagraph);
             toggleHiddenButton(favouriteMessageButton);
         }
    
-        createMessageBox.innerHTML = `<p>${createMessage(mantras)}</p>`;
+        messageHtmlParagraph.innerText = createMessage(mantras);
+
+        
+        
+        // if (imgBuddha.classList.contains('img-buddha')) {
+        //     imgBuddha.remove();
+        // }
+        // createMessageBox.appendChild(newHtmlParagraph)
+        // newHtmlParagraph.innerText = createMessage(mantras);
+
+
     }
 }
 
@@ -72,19 +112,33 @@ function toggleHiddenButton(button) {
     button.classList.toggle('hidden');
 }
 
-function favouriteMessage() {
+function favouriteMessage() {favouriteMessageButton = document.querySelector('.save-message');
     if (!savedMessages.includes(currentMessage)) {
         savedMessages.push(currentMessage);
     }
 }
 
 function viewFavouriteMessages() {
-    messageContainer.innerHTML = '';
+    favouriteSection.innerHTML = '';
     for (var i = 0; i < savedMessages.length; i++) {
         // console.log(savedMessages[i]);
-        messageContainer.innerHTML += `
+        
+        // newHtmlSection.classList = ``;
+        // newHtmlButton.classList = ``;
+
+        // newHtmlSection.classList = `message-container ${[i]}`;
+        // newHtmlButton.classList = `delete-message ${[i]}`;
+
+        // messageContainer.appendChild(newHtmlSection);
+        // newHtmlSection.appendChild(newHtmlParagraph);
+        // newHtmlParagraph.innerText = savedMessages[i];
+        // newHtmlSection.appendChild = newHtmlButton;
+        // newHtmlButton.innerText = 'Delete';
+        
+        favouriteSection.innerHTML += `
             <section class="message-container">
                 <p>${savedMessages[i]}</p>
+                <button class="delete-message ${[i]}">Delete</button>
             </section>`;
     }
 }
@@ -98,9 +152,42 @@ function swapSubTitle() {
 }
 
 function viewHome() {
-    messageContainer.innerHTML = `
-    <section class="message-container">
-        <div class="display-message"><img src="./assets/meditate.svg" alt="enlightened buddha"></div>
-        <button class="hidden save-message">Save Message</button>
-    </section>`;
+    
+    // messageContainer.innerHTML = `
+    // <section class="message-container">
+    //     <div class="display-message"><img class="img-buddha" src="./assets/meditate.svg" alt="enlightened buddha"></div>
+    //     <button class="hidden save-message">Favourite</button>
+    // </section>`;
+    // messageContainer.innerHTML = '';
+
+    
+    // var removeBox = document.querySelectorAll('.message-container');
+    // console.log(removeBox)
+    // for (var i = 0; i < removeBox.length; i++) {
+
+    //     messageContainer.removeChild(removeBox[i])
+    // }
+    // console.log(removeBox)
+
+    // messageContainer.appendChild(newHtmlSection);
+    // newHtmlSection.classList = `message-container`;
+    // newHtmlSection.appendChild(newHtmlDiv);
+    // newHtmlDiv.classList = `display-message`;
+    // newHtmlDiv.appendChild(newHtmlImg);
+    // newHtmlImg.classList = `img-buddha`; 
+    // newHtmlImg.alt = `enlightened buddha`;
+    // newHtmlImg.src = `./assets/meditate.svg`;
+    // newHtmlSection.appendChild = newHtmlButton;
+    // newHtmlButton.classList = `hidden save-message`;
+    // newHtmlButton.innerText = 'Favourite';
+}
+
+function deleteFavourite() {
+    // deleteFavouriteButton = document.querySelector('.delete-message');
+    console.log('test')
+    // for (var i = 0; i < savedMessages.length; i++) {
+    //     if (event)
+    // }
+    console.dir(deleteFavouriteButton.target)
+    // event.target.parentNode;
 }
