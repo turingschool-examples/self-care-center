53a5e21c0b045d1d18135a56a85e5eec98adbe3f
Taranveer Singh <taranveersingh93@gmail.com>
2023-04-06 21:52:56 -0400

    Remove pseudocode

--- main.js
+++ main.js
@@ -96,36 +96,3 @@ function renderCurrentMessage() {
     hideDomElement(meditateIcon);
     showDomElement(displayMessage);
 }
-
-/*
-import the DOM elements
-create getRandomIndex
-create variables currentMantra or currentAffirmation
-
-makeClones:
-make a safeMantras and safeAffirmations
-make cloneMantras and cloneAffirmations.
-Use cloned arrays for displays and splice them
-
-reassignCurrent:
-var randomIndex = getRandomIndex from the array
-if(cloneArray.length)
-if(notificationStyle is in classlist)
-remove that style,
-var currentMantra = Array[index]
-splice the array using the same index
-else 
-run (refillMessages)
-(notify the message) and (clone the safe array.)
-Notify the message by reassigning current. 
-add a style class for notification to be different.
-
-renderCurrent
-
-displayMessage.innerText = currentMantra or currentAffirmation
-addClass hidden to the meditate icon
-removeClass hidden from displayMessage
-
-when button click, depending on what user selected, 
-invoke reassignCurrent and renderCurrent versions
-*/ 
