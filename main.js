c802f5277cf32a3e372363552fe724a2c4b4cbd0
Hollis Vohr <hollis.vohr@gmail.com>
2023-04-07 14:05:18 -0600

    Uncomment the affirmations array

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ var affirmations = [
     "I forgive myself and set myself free.",
     "I believe I can be all that I want to be.",
     "I am in the process of becoming the best version of myself.",
-    // "I have the freedom & power to create the life I desire.",
-    // "I choose to be kind to myself and love myself unconditionally.",
-    // "My possibilities are endless.",
-    // "I am worthy of my dreams.",
-    // "I deserve to be healthy and feel good.",
-    // "I am full of energy and vitality and my mind is calm and peaceful.",
-    // "Every day I am getting healthier and stronger.",
-    // "I honor my body by trusting the signals that it sends me.",
-    // "I manifest perfect health by making smart choices."
+    "I have the freedom & power to create the life I desire.",
+    "I choose to be kind to myself and love myself unconditionally.",
+    "My possibilities are endless.",
+    "I am worthy of my dreams.",
+    "I deserve to be healthy and feel good.",
+    "I am full of energy and vitality and my mind is calm and peaceful.",
+    "Every day I am getting healthier and stronger.",
+    "I honor my body by trusting the signals that it sends me.",
+    "I manifest perfect health by making smart choices."
 ];
 
 var mantras = [
