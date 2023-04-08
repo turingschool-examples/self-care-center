1c2c79733fc99b3f2632d67d0946a9483b352eea
Jamie Caudill <j.caudill7177@gmail.com>
2023-04-08 15:40:09 -0700

    Clean up

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var mantras = [
     'Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.',
-    'Don’t let yesterday take up too much of today.',
+    "Don’t let yesterday take up too much of today.",
     'Every day is a second chance.',
     'Tell the truth and love everyone.',
     'I am free from sadness.',
--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var favoriteMessage = document.querySelector('.favorite-message');
 var favoriteBox = document.querySelector('.favorite-box');
 // button variables
 var removeBtn = document.querySelector('.remove-btn');
-var receiveBtn = document.querySelector('.receive-btn');
+var receiveBtn = document.querySelector('.receive-btn-section');
 var affirmationBtn = document.querySelector('.affirmation-btn');
 var mantraBtn = document.querySelector('.mantra-btn');
 var favBtn = document.querySelector('.favorite');
@@ -20,39 +20,6 @@ var favoritesPageBtn = document.querySelector('.favorites-page-btn');
 // arrays
 var favorites = [];
 var currentMessage = [];
-var mantras = [
-    'Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.',
-    "Dont let yesterday take up too much of today.",
-    'Every day is a second chance.',
-    'Tell the truth and love everyone.',
-    'I am free from sadness.',
-    'I am enough.',
-    'In the beginning it is you, in the middle it is you and in the end it is you.',
-    'I love myself.',
-    'I am present now.',
-    'Inhale the future, exhale the past.',
-    'This too shall pass.',
-    'Yesterday is not today.',
-    'The only constant is change.',
-    'Onward and upward.',
-    'I am the sky, the rest is weather.'
-    ];
-    
-    var affirmations = [
-    'I forgive myself and set myself free.',
-    'I believe I can be all that I want to be.',
-    'I am in the process of becoming the best version of myself.',
-    'I have the freedom & power to create the life I desire.',
-    'I choose to be kind to myself and love myself unconditionally.',
-    'My possibilities are endless.',
-    'I am worthy of my dreams.',
-    'I am enough.',
-    'I deserve to be healthy and feel good.',
-    'I am full of energy and vitality and my mind is calm and peaceful.',
-    'Every day I am getting healthier and stronger.',
-    'I honor my body by trusting the signals that it sends me.',
-    'I manifest perfect health by making smart choices.'
-    ];
 
 // Event Listeners
 
