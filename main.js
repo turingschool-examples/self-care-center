f3f67406012b4121c37499acfe3cbd5f350849cf
Ann Hochworter <ann.hochworter@gmail.com>
2023-07-18 12:57:18 -0600

    Add the very basics to each component of the self-care files

---
+++ main.js
@@ -0,0 +1,15 @@
+//Create variables targeting the relevant DOM elements here👇
+
+//BUTTONS:👇
+//Radio button to select affirmation
+//Radio button to select mantra
+//Receive message
+
+//VIEWS:👇
+//Box with message should toggle between showing the icon and showing the message.
+
+//Antything provided?
+
+//Add your event listeners here 👇
+
+//Create your event handlers and other functions here👇
