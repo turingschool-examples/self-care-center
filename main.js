bc119dbf1dfefb0f64bf33459eb3f54b5d97be8c
Patrick Eitel <patrickeitel12@gmail.com>
2023-07-21 18:46:56 -0700

    refactor: add id tag to main button. Add event listener and planned out future functions within said event listener. Punctuated code.

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
 var affirmationButton = document.querySelector("#radio1");
 var mantraButton = document.querySelector("#radio2");
-
+var mainButton = document.querySelector("#main button");
 
 
 
 affirmationButton.addEventListener("click", function(event) {
-mantraButton.checked = false;
-});
-mantraButton.addEventListener("click", function(event) {
-affirmationButton.checked = false;
-});
+    mantraButton.checked = false;
+  });
+  
+  mantraButton.addEventListener("click", function(event) {
+    affirmationButton.checked = false;
+  });
 // Could we do this as one conditional?
+
+mainButton.addEventListener("click", function(event) {
+    hidePicture();
+  
+    if (affirmationButton.checked === false && mantraButton.checked === false) {
+      showErrorMessage();
+    }
+  
+    if (affirmationButton.checked === true) {
+      showAffirmationMessage();
+    }
+  
+    if (mantraButton.checked === true) {
+      showMantraMessage();
+    }
+  });
+// This is an idea for the end goal of the site. None of these are written yet!
