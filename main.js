fa49babc347ec1e642a3edee54e73d6506d9aa1d
Ricky Tran <rickytran.dev@gmail.com>
2023-10-28 14:59:48 -0600

    Feat: added styling to buttons

--- main.js
+++ main.js
@@ -1,40 +1,56 @@
 // Event Listener
-var recieveMsgBtn = document.querySelector('button[type="button"]')
+var recieveMsgBtn = document.querySelector('#receive-btn')
 var affirmationBtn = document.querySelector("#affirmation")
+var clearBtn = document.querySelector('.clear-btn')
 var mantraBtn = document.querySelector("#mantra")
 
 // Event Handler
 recieveMsgBtn.addEventListener('click', createMessage);
 affirmationBtn.addEventListener('click', handleRadioClick)
 mantraBtn.addEventListener('click', handleRadioClick)
+clearBtn.addEventListener('click', clearMessage)
 
 // Functions
-var randomIndex;
+var id;
 
 function getRandomIndex(array) {
     randomIndex = Math.floor(Math.random() * array.length)  
     return  randomIndex
   }
 
-var id;
-
 function handleRadioClick(event) {
     id = event.target.id;
-    return id
+    recieveMsgBtn.classList.add('button-hover')
 }
 
 function createMessage() {
+
     switch (id) {
         case "affirmation":
-            getRandomIndex(affirmations)
-            document.querySelector('.message').innerText = affirmations[randomIndex]
+            var randomIndex = getRandomIndex(affirmations)
+            document.querySelector('.message').innerText = affirmations[randomIndex];
+            show(clearBtn);
             break;
         case "mantra":
-            getRandomIndex(mantras)
+            var randomIndex = getRandomIndex(mantras)
             document.querySelector('.message').innerText = mantras[randomIndex];
+            show(clearBtn);
             break;
         default:
-            alert('Error: Please select a radio button.')
+            alert('Please select an affirmation or a mantra.')
             break;
     }
 }
+
+function clearMessage() {
+    document.querySelector('.message').innerHTML = '<img src="/assets/meditate.svg" alt="meditation emoji">'
+    hide(clearBtn)
+}
+
+function show(element) {
+    element.classList.remove('hidden');
+  }
+    
+  function hide(element) {
+    element.classList.add('hidden');
+  }
