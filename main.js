b344086aa14b2aaaa9eed1a1ab99db2df14651d6
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2024-02-14 15:47:06 -0700

    Add psuedocode to js file

---
+++ main.js
@@ -0,0 +1,28 @@
+
+// Restate the goal: 
+// I need to build a web app that will allow users to recieve a self care 
+// message. They should be able to select the type of message they want 
+// to recieve, click a button, and then see a randomly generated message
+// Data Types: 
+// I need have a data model - this will be an array or arrays that store the 
+// messages in them
+// I will be working with strings - these will be the messages
+// I will be working with radio buttons
+// Questiions:
+// is it better to use innerHTML or createElement and then appendChild when 
+// displaying something to the DOM?
+// should I be using display: hidden for the image currently in the output
+// how do I read the value of a radio button? or see if it is checked?
+// Psuedocode:
+// I need 2 arrays that will store the message strings
+// I need a function that will randomly choose a message from the arrays
+// I need a function that will change the innerHTML of the output box
+// I need querySelectors that will target the radio buttons and see if there
+// value is checked
+// I need a querySelector that will target the message button
+// I need a querySelector that willt target the output box
+// I need a class of hidden in css to show and hide the image in the output box
+
+
+
+
