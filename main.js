46369a8d167f036bbce6f68efc3d5b5003214a15
Zach Wolek <zach.wolek@gmail.com>
2024-02-17 15:32:48 -0600

    Complete MVP

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
 function getRandomIndex(array) {
-    return Math.floor(Math.random() * array.length);
+    var randomIndex = Math.floor(Math.random() * array.length);
+    var randomMessage = array[randomIndex]
+    return randomMessage
   }
 
+//Query Selectors
+  var submitButton = document.querySelector("#receive-message-button");
+  var mantraChoice = document.querySelector("#radio-mantra")
+  var affirmationChoice = document.querySelector("#radio-affirmation")
+  var meditationImage = document.querySelector("#meditation-image");
+  var meditationBox = document.querySelector(".meditation-box");
+  //random variable arrays
 
 
-  
+  //event listeners
+  submitButton.addEventListener('click', displaySelfCare)
+
+
+  //functions
+  function displaySelfCare(){
+    meditationImage.classList.add(".meditation-hidden");
+      if (mantraChoice.checked) {
+          var randomMantra = getRandomIndex(mantras);
+         meditationBox.innerHTML = `${randomMantra}`
+      } else if (affirmationChoice.checked) {
+          var randomAffirmation = getRandomIndex(affirmations);
+          meditationBox.innerHTML = `${randomAffirmation}`;
+      };
+  }
